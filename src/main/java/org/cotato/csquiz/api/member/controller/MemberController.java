5e69b8f3dd3425dd16651184b7473172d371ae2c
gikhoon <ghikhoon@naver.com>
2024-07-11 14:43:53 +0900

    feat: 멤버 프로필 사진 수정 기능 구현
    feat: 멤버 프로필 사진 수정 기능 구현

--- src/main/java/org/cotato/csquiz/api/member/controller/MemberController.java
+++ src/main/java/org/cotato/csquiz/api/member/controller/MemberController.java
@@ -8,11 +8,13 @@ import org.cotato.csquiz.api.admin.dto.MemberInfoResponse;
 import org.cotato.csquiz.api.member.dto.MemberMyPageInfoResponse;
 import org.cotato.csquiz.api.member.dto.UpdatePasswordRequest;
 import org.cotato.csquiz.api.member.dto.UpdatePhoneNumberRequest;
+import org.cotato.csquiz.api.member.dto.UpdateProfileImageRequest;
 import org.cotato.csquiz.common.config.jwt.JwtTokenProvider;
+import org.cotato.csquiz.common.error.exception.ImageException;
 import org.cotato.csquiz.domain.auth.service.MemberService;
-import org.springframework.context.annotation.Description;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,7 +53,17 @@ public class MemberController {
             @RequestHeader("Authorization") String authorizationHeader,
             @RequestBody @Valid UpdatePhoneNumberRequest request) {
         String accessToken = jwtTokenProvider.getBearer(authorizationHeader);
-        memberService.updatePhoneNumber(accessToken,request.phoneNumber());
+        memberService.updatePhoneNumber(accessToken, request.phoneNumber());
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "멤버 프로필 사진 수정", description = "멤버 프로필 사진 수정하기")
+    @PatchMapping(value = "/profile-image", consumes = "multipart/form-data")
+    public ResponseEntity<Void> updateProfileImage(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @ModelAttribute @Valid UpdateProfileImageRequest request) throws ImageException {
+        String accessToken = jwtTokenProvider.getBearer(authorizationHeader);
+        memberService.updateMemberProfileImage(accessToken, request.image());
         return ResponseEntity.noContent().build();
     }
 
