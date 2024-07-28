08396cfc21bdd7b77312b28a1c2030fe752b92ff
youth <boysoeng@naver.com>
2024-07-28 19:45:17 +0900

    style: 코드 간격 조정

--- src/main/java/org/cotato/csquiz/domain/auth/enums/EmailType.java
+++ src/main/java/org/cotato/csquiz/domain/auth/enums/EmailType.java
@@ -3,11 +3,11 @@ package org.cotato.csquiz.domain.auth.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
 public enum EmailType {
-    SIGNUP("$sign-up","exist","코드 일치 성공"),
-    UPDATE_PASSWORD("$update-pwd","exist","코드 요청 후 검증 요청을 보내지 않음")
+    SIGNUP("$sign-up", "exist", "코드 일치 성공"),
+    UPDATE_PASSWORD("$update-pwd", "exist", "코드 요청 후 검증 요청을 보내지 않음")
     ;
 
     private final String keyPrefix;
