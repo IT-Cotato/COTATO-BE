5a2fe5c886a525cf2eae3676b6e417fcfeb967fb
gikhoon <ghikhoon@naver.com>
2024-08-01 16:56:32 +0900

    feat: 세션 출석 기한 유효성 방식 변경
    - 기존 NotNull에서 Compact생성자로 디폴트 값 방식으로 변경

--- src/main/java/org/cotato/csquiz/api/attendance/dto/AttendanceDeadLineDto.java
+++ src/main/java/org/cotato/csquiz/api/attendance/dto/AttendanceDeadLineDto.java
@@ -1,15 +1,26 @@
 package org.cotato.csquiz.api.attendance.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
+import java.util.Objects;
+import lombok.Builder;
 
 public record AttendanceDeadLineDto(
-        @NotNull
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         LocalTime startTime,
-        @NotNull
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         LocalTime endTime
 ) {
+    public static final LocalTime DEFAULT_START_TIME = LocalTime.of(19, 0);
+    public static final LocalTime DEFAULT_END_TIME = LocalTime.of(19, 20);
+
+    @Builder
+    public AttendanceDeadLineDto {
+        if (Objects.isNull(startTime)) {
+            startTime = DEFAULT_START_TIME;
+        }
+        if (Objects.isNull(endTime)) {
+            endTime = DEFAULT_END_TIME;
+        }
+    }
 }
