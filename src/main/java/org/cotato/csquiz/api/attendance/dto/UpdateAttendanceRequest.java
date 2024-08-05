4016213e8c91d9503cba00fc0c2bd23a03bc7899
gikhoon <ghikhoon@naver.com>
2024-08-05 21:40:05 +0900

    fix: 세션 추가 양식 변경을 통한 역직렬화 문제 해결
    - 중첩 레코드 삭제
    - 출석 마감기한 컴팩트 생성자 설정
    
    fix: 세션 추가 양식 변경을 통한 역직렬화 문제 해결
    
    - 중첩 레코드 삭제
    - 출석 마감기한 컴팩트 생성자 설정

--- src/main/java/org/cotato/csquiz/api/attendance/dto/UpdateAttendanceRequest.java
+++ src/main/java/org/cotato/csquiz/api/attendance/dto/UpdateAttendanceRequest.java
@@ -1,16 +1,22 @@
 package org.cotato.csquiz.api.attendance.dto;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalTime;
+import java.util.Objects;
 import org.cotato.csquiz.domain.attendance.embedded.Location;
 
 public record UpdateAttendanceRequest(
         @NotNull
         Long attendanceId,
         Location location,
-
-        @Valid
-        @NotNull
         AttendanceDeadLineDto attendanceDeadLine
 ) {
+    public static final LocalTime DEFAULT_START_TIME = LocalTime.of(19, 0);
+    public static final LocalTime DEFAULT_END_TIME = LocalTime.of(19, 20);
+
+    public UpdateAttendanceRequest {
+        if (Objects.isNull(attendanceDeadLine)) {
+            attendanceDeadLine = new AttendanceDeadLineDto(DEFAULT_START_TIME, DEFAULT_END_TIME);
+        }
+    }
 }
--- src/main/java/org/cotato/csquiz/domain/attendance/embedded/Location.java
+++ src/main/java/org/cotato/csquiz/domain/attendance/embedded/Location.java
@@ -12,4 +12,13 @@ public class Location {
 
     private Double latitude;
     private Double longitude;
+
+    public Location(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public static Location of(Double latitude, Double longitude) {
+        return new Location(latitude, longitude);
+    }
 }
