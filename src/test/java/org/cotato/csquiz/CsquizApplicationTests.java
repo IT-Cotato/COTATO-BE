075155833dc2aa2fc90e8d099aeeb6ab24b72054
youth <boysoeng@naver.com>
2024-08-09 01:40:18 +0900

    chore: 사용하지 않는 테스트 코드 주석 제거

--- src/test/java/org/cotato/csquiz/CsquizApplicationTests.java
+++ src/test/java/org/cotato/csquiz/CsquizApplicationTests.java
@@ -1,13 +1,8 @@
 package org.cotato.csquiz;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CsquizApplicationTests {
-
-//    @Test
-//    void contextLoads() {
-//    }
 
 }
