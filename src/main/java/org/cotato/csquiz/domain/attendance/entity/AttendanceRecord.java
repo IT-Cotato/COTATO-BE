ee4426bf450f10e39a675cd5a6a2f699272ca0b2
youth <boysoeng@naver.com>
2024-08-08 11:07:38 +0900

    style: 사용하지 않는 import 문 제거

--- src/main/java/org/cotato/csquiz/domain/attendance/entity/AttendanceRecord.java
+++ src/main/java/org/cotato/csquiz/domain/attendance/entity/AttendanceRecord.java
@@ -13,7 +13,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.cotato.csquiz.common.entity.BaseTimeEntity;
