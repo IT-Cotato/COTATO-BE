69749f1acad20332d06a94826027b2a8b1fd873e
yunhacandy <yunhacandy@gmail.com>
2024-08-13 21:42:20 +0900

    feat: 프로젝트 관련 Entity 구현

---
+++ src/main/java/org/cotato/csquiz/domain/generation/entity/Project.java
@@ -0,0 +1,46 @@
+package org.cotato.csquiz.domain.generation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private Long id;
+
+    @Column(name = "project_name")
+    private String name;
+
+    @Column(name = "project_introduction")
+    private String introduction;
+
+    @Column(name = "github_url")
+    private String githubUrl;
+
+    @Column(name = "behance_url")
+    private String behanceUrl;
+
+    @Column(name = "generation_id", nullable = false, unique = true)
+    private Long generationId;
+
+    @Builder
+    public Project(String name, String introduction, String githubUrl, String behanceUrl, Long generationId) {
+        this.name = name;
+        this.introduction = introduction;
+        this.githubUrl = githubUrl;
+        this.behanceUrl = behanceUrl;
+        this.generationId = generationId;
+    }
+}
