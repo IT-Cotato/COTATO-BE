bf372e15957929027531370b777526110121edee
Yun Ha Park <127095481+yunhacandy@users.noreply.github.com>
2024-08-14 12:43:57 +0900

    Feature: 프로젝트 관련 테이블 생성 (#93)
    * feat: 프로젝트 관련 Entity 구현
    
    * refactor: 프로젝트 관련 엔티티 수정
    
    - 프로젝트 서비스 url 칼럼 추가
    - unique 제약 조건 삭제
    
    * refactor: builder에 project_url 추가
    
    * style: 코드 스타일 통일

---
+++ src/main/java/org/cotato/csquiz/domain/generation/entity/ProjectImage.java
@@ -0,0 +1,44 @@
+package org.cotato.csquiz.domain.generation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.cotato.csquiz.common.entity.S3Info;
+import org.cotato.csquiz.domain.generation.enums.ProjectImageType;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProjectImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_image_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "project_image_type")
+    private ProjectImageType projectImageType;
+
+    @Embedded
+    private S3Info s3Info;
+
+    @Column(name = "project_id", nullable = false)
+    private Long projectId;
+
+    @Builder
+    public ProjectImage(ProjectImageType projectImageType, S3Info s3Info, Long projectId) {
+        this.projectImageType = projectImageType;
+        this.s3Info = s3Info;
+        this.projectId = projectId;
+    }
+}
