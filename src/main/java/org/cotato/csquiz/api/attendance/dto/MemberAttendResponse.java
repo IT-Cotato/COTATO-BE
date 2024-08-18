d9d3e3bef43a303c8f408903e21b2698e90c04b7
youth <boysoeng@naver.com>
2024-08-18 20:03:51 +0900

    feat: 부원 출결 기록 반환 필드 출석 PK 추가

--- src/main/java/org/cotato/csquiz/api/attendance/dto/MemberAttendResponse.java
+++ src/main/java/org/cotato/csquiz/api/attendance/dto/MemberAttendResponse.java
@@ -2,6 +2,7 @@ package org.cotato.csquiz.api.attendance.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
+import org.cotato.csquiz.domain.attendance.entity.Attendance;
 import org.cotato.csquiz.domain.attendance.entity.AttendanceRecord;
 import org.cotato.csquiz.domain.attendance.enums.AttendanceOpenStatus;
 import org.cotato.csquiz.domain.attendance.enums.AttendanceResult;
@@ -9,10 +10,12 @@ import org.cotato.csquiz.domain.attendance.enums.AttendanceType;
 import org.cotato.csquiz.domain.generation.entity.Session;
 
 public record MemberAttendResponse(
-        @Schema(description = "멤버 PK")
-        Long memberId,
         @Schema(description = "세션 PK")
         Long sessionId,
+        @Schema(description = "출석 PK")
+        Long attendanceId,
+        @Schema(description = "멤버 PK")
+        Long memberId,
         @Schema(description = "세션 타이틀", example = "3주차 세션")
         String sessionTitle,
         @Schema(description = "세션 날짜")
@@ -28,8 +31,9 @@ public record MemberAttendResponse(
 ) {
     public static MemberAttendResponse closedAttendanceResponse(Session session, AttendanceRecord attendanceRecord) {
         return new MemberAttendResponse(
-                attendanceRecord.getMemberId(),
                 session.getId(),
+                attendanceRecord.getAttendance().getId(),
+                attendanceRecord.getMemberId(),
                 session.getTitle(),
                 session.getSessionDate(),
                 AttendanceOpenStatus.CLOSED,
@@ -38,10 +42,11 @@ public record MemberAttendResponse(
         );
     }
 
-    public static MemberAttendResponse openedAttendanceResponse(Session session, Long memberId) {
+    public static MemberAttendResponse openedAttendanceResponse(Attendance attendance, Session session, Long memberId) {
         return new MemberAttendResponse(
-                memberId,
                 session.getId(),
+                attendance.getId(),
+                memberId,
                 session.getTitle(),
                 session.getSessionDate(),
                 AttendanceOpenStatus.OPEN,
