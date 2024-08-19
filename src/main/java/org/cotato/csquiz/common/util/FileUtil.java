a8db366db043ea19b98f47355ebe476db49b630e
gikhoon <ghikhoon@naver.com>
2024-08-19 10:58:43 +0900

    refactor: 대문자 파일 확장자 이미지 추가 방식 변경
    - 리스트에 추가 -> toLowerCase도 변경

--- src/main/java/org/cotato/csquiz/common/util/FileUtil.java
+++ src/main/java/org/cotato/csquiz/common/util/FileUtil.java
@@ -7,7 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileUtil {
 
-    private static final List<String> IMAGE_FILE_EXTENSIONS = List.of("png", "jpg", "jpeg", "heif", "PNG", "JPG", "JPEG", "HEIF");
+    private static final List<String> IMAGE_FILE_EXTENSIONS = List.of("png", "jpg", "jpeg", "heif");
 
     public static String extractFileExtension(MultipartFile file) throws ImageException {
         String originalFilename = file.getOriginalFilename();
@@ -19,6 +19,6 @@ public class FileUtil {
     }
 
     public static boolean isImageFileExtension(String fileExtension) {
-        return IMAGE_FILE_EXTENSIONS.contains(fileExtension);
+        return IMAGE_FILE_EXTENSIONS.contains(fileExtension.toLowerCase());
     }
 }
