96736c8056b0edb3984cf565e3a3bd136281cd2d
youth <boysoeng@naver.com>
2024-08-20 10:35:58 +0900

    feat: 출결 이벤트 서비스 레이어 구현

--- src/main/java/org/cotato/csquiz/common/sse/SseService.java
+++ src/main/java/org/cotato/csquiz/common/sse/SseService.java
@@ -1,29 +1,26 @@
 package org.cotato.csquiz.common.sse;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Slf4j
-@Component
+@Service
 @RequiredArgsConstructor
 public class SseService {
 
-    private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
-    private final Map<Long, SseEmitter> attendances = new ConcurrentHashMap<>();
+    private static final Long DEFAULT_TIMEOUT = 60 * 1000 * 60L;
+
+    private final SseAttendanceRepository sseAttendanceRepository;
     private final SseSender sseSender;
 
-    public SseEmitter subscribeAttendance(final Long memberId) throws IOException {
+    public SseEmitter subscribeAttendance(final Long memberId) {
         SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
 
-        saveEmitter(memberId, sseEmitter);
         setBaseEmitterConfiguration(memberId, sseEmitter);
+        sseAttendanceRepository.save(memberId, sseEmitter);
 
-        sseSender.sendAttendanceConnected(sseEmitter);
         sseSender.sendInitialAttendanceStatus(sseEmitter);
 
         return sseEmitter;
@@ -32,21 +29,12 @@ public class SseService {
     private void setBaseEmitterConfiguration(Long memberId, SseEmitter sseEmitter) {
         sseEmitter.onCompletion(() -> {
             log.info("---- [memberId]: {} on completion callback ----", memberId);
-            this.attendances.remove(memberId, sseEmitter);
+            sseAttendanceRepository.deleteById(memberId);
         });
 
         sseEmitter.onTimeout(() -> {
             log.info("---- [memberId]: {} on timeout callback ----", memberId);
             sseEmitter.complete();
         });
-    }
-
-    private void saveEmitter(Long memberId, SseEmitter sseEmitter) {
-        this.attendances.computeIfPresent(memberId, (key, existing) -> {
-            existing.complete();
-            return sseEmitter;
-        });
-
-        this.attendances.putIfAbsent(memberId, sseEmitter);
     }
 }
