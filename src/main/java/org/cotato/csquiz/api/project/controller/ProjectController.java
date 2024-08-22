47b32de01e313af3587ed5bcdf6b819b0b023c0d
youth <boysoeng@naver.com>
2024-08-23 00:24:34 +0900

    fix: 사진 업로드를 위한 request 타입 변경

--- src/main/java/org/cotato/csquiz/api/project/controller/ProjectController.java
+++ src/main/java/org/cotato/csquiz/api/project/controller/ProjectController.java
@@ -14,9 +14,9 @@ import org.cotato.csquiz.domain.generation.service.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,8 +41,8 @@ public class ProjectController {
     }
 
     @Operation(summary = "프로젝트 등록 API")
-    @PostMapping
-    public ResponseEntity<CreateProjectResponse> createProject(@RequestBody @Valid CreateProjectRequest request)
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<CreateProjectResponse> createProject(@ModelAttribute @Valid CreateProjectRequest request)
             throws ImageException {
         return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(request));
     }
