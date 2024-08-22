5108422c45fb946f955cc455c53b50848ec33789
gikhoon <ghikhoon@naver.com>
2024-08-23 01:18:42 +0900

    feat: 프로젝트 리스트 반환 기능에 url 필드 추가

--- src/main/java/org/cotato/csquiz/domain/generation/service/ProjectMemberService.java
+++ src/main/java/org/cotato/csquiz/domain/generation/service/ProjectMemberService.java
@@ -1,6 +1,5 @@
 package org.cotato.csquiz.domain.generation.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.cotato.csquiz.api.project.dto.ProjectMemberRequest;
