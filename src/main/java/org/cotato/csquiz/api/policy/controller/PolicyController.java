7f76030b54a4b1d1ec2f1d0531d43868c7f3b1f7
youth <boysoeng@naver.com>
2024-09-03 23:12:05 +0900

    style: 사용하지 않는 import 제거

--- src/main/java/org/cotato/csquiz/api/policy/controller/PolicyController.java
+++ src/main/java/org/cotato/csquiz/api/policy/controller/PolicyController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "회원 정책 API")
