08894ac41ec56b09ff614432d746ac44dbab2053
Youth <109585620+Youthhing@users.noreply.github.com>
2024-09-03 23:23:05 +0900

    Feature: 회원 가입 시 정책 동의 로직 추가 구현 (#141)
    * feat: 회원 정책 동의 메서드 파라미터 변경
    
    * feat: 회원 가입 시 필요 정책 동의 기능 추가
    
    * docs: 스웨거 API 명세 구체화
    
    * style: 사용하지 않는 import 제거

--- src/main/java/org/cotato/csquiz/api/auth/dto/JoinRequest.java
+++ src/main/java/org/cotato/csquiz/api/auth/dto/JoinRequest.java
@@ -1,9 +1,12 @@
 package org.cotato.csquiz.api.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.util.List;
+import org.cotato.csquiz.api.policy.dto.CheckPolicyRequest;
 
 public record JoinRequest(
         @Email(message = "올바른 형식의 메일 주소를 입력해주세요.")
@@ -16,6 +19,9 @@ public record JoinRequest(
         String name,
         @NotNull(message = "전화번호를 입력해주세요.")
         @Size(min = 11, max = 11, message = "'-'없이 11자리의 전화번호를 입력해주세요.")
-        String phoneNumber
+        String phoneNumber,
+
+        @Schema(description = "동의 표시한 정책 목록")
+        List<CheckPolicyRequest> policies
 ) {
 }
