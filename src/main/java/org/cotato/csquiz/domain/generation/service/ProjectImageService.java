b7319b5ad79f3e759ed107dabcc16d8f00de66c0
gikhoon <ghikhoon@naver.com>
2024-09-11 18:46:34 +0900

    refactor: 파일 변환 메소드 위치 이동
    - S3Uploader -> FileUtil

--- src/main/java/org/cotato/csquiz/domain/generation/service/ProjectImageService.java
+++ src/main/java/org/cotato/csquiz/domain/generation/service/ProjectImageService.java
@@ -1,5 +1,7 @@
 package org.cotato.csquiz.domain.generation.service;
 
+import static org.cotato.csquiz.common.util.FileUtil.*;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.cotato.csquiz.common.entity.S3Info;
 import org.cotato.csquiz.common.error.exception.ImageException;
 import org.cotato.csquiz.common.s3.S3Uploader;
-import org.cotato.csquiz.common.util.FileUtil;
 import org.cotato.csquiz.domain.generation.entity.ProjectImage;
 import org.cotato.csquiz.domain.generation.repository.ProjectImageRepository;
 import org.springframework.stereotype.Service;
@@ -27,11 +28,11 @@ public class ProjectImageService {
             throws ImageException {
         List<ProjectImage> newImages = new ArrayList<>();
 
-        File webpLogoImage = FileUtil.convertToWebp(s3Uploader.convert(logoImage));
+        File webpLogoImage = convertToWebp(convert(logoImage));
         S3Info logoImageInfo = s3Uploader.uploadFiles(webpLogoImage, PROJECT_IMAGE);
         newImages.add(ProjectImage.logoImage(logoImageInfo, projectId));
 
-        File webpThumbNailImage = FileUtil.convertToWebp(s3Uploader.convert(thumbNailImage));
+        File webpThumbNailImage = convertToWebp(convert(thumbNailImage));
         S3Info thumbNailInfo = s3Uploader.uploadFiles(webpThumbNailImage, PROJECT_IMAGE);
         newImages.add(ProjectImage.thumbNailImage(thumbNailInfo, projectId));
 
