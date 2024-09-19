1a8f5afb30044aa19b873b96d37579710f4fffb1
yunhacandy <yunhacandy@gmail.com>
2024-09-19 23:19:23 +0900

    refactor: 이미지 존재 여부 검증 메서드 변경

--- src/main/java/org/cotato/csquiz/domain/generation/repository/ProjectImageRepository.java
+++ src/main/java/org/cotato/csquiz/domain/generation/repository/ProjectImageRepository.java
@@ -11,9 +11,5 @@ public interface ProjectImageRepository extends JpaRepository<ProjectImage, Long
     List<ProjectImage> findAllByProjectId(Long projectId);
     List<ProjectImage> findAllByProjectIdInAndProjectImageType(List<Long> projectIds, ProjectImageType projectImageType);
 
-    @Query("SELECT p FROM ProjectImage p WHERE p.projectId = :projectId AND p.projectImageType = 'LOGO'")
-    Optional<ProjectImage> findLogoImageByProjectId(Long projectId);
-
-    @Query("SELECT p FROM ProjectImage p WHERE p.projectId = :projectId AND p.projectImageType = 'THUMBNAIL'")
-    Optional<ProjectImage> findThumbnailImageByProjectId(Long projectId);
+    boolean existsByProjectIdAndProjectImageType(Long projectId, ProjectImageType projectImageType);
 }
