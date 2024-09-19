f65bf0af0d220749ba62a17ca754808b29bce7ed
yunhacandy <yunhacandy@gmail.com>
2024-09-19 23:20:10 +0900

    refactor: 이미지 존재 여부 검증 메서드 분리

--- src/main/java/org/cotato/csquiz/domain/generation/repository/ProjectImageRepository.java
+++ src/main/java/org/cotato/csquiz/domain/generation/repository/ProjectImageRepository.java
@@ -1,11 +1,9 @@
 package org.cotato.csquiz.domain.generation.repository;
 
 import java.util.List;
-import java.util.Optional;
 import org.cotato.csquiz.domain.generation.entity.ProjectImage;
 import org.cotato.csquiz.domain.generation.enums.ProjectImageType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ProjectImageRepository extends JpaRepository<ProjectImage, Long> {
     List<ProjectImage> findAllByProjectId(Long projectId);
--- src/main/java/org/cotato/csquiz/domain/generation/service/ProjectImageService.java
+++ src/main/java/org/cotato/csquiz/domain/generation/service/ProjectImageService.java
@@ -6,7 +6,6 @@ import static org.cotato.csquiz.common.util.FileUtil.convertToWebp;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.cotato.csquiz.common.entity.S3Info;
 import org.cotato.csquiz.common.error.ErrorCode;
@@ -14,6 +13,7 @@ import org.cotato.csquiz.common.error.exception.AppException;
 import org.cotato.csquiz.common.error.exception.ImageException;
 import org.cotato.csquiz.common.s3.S3Uploader;
 import org.cotato.csquiz.domain.generation.entity.ProjectImage;
+import org.cotato.csquiz.domain.generation.enums.ProjectImageType;
 import org.cotato.csquiz.domain.generation.repository.ProjectImageRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,15 +31,8 @@ public class ProjectImageService {
     public void createProjectImage(Long projectId, MultipartFile logoImage, MultipartFile thumbNailImage,
                                    List<MultipartFile> detailImages) throws ImageException {
 
-        Optional<ProjectImage> existingLogoImage = projectImageRepository.findLogoImageByProjectId(projectId);
-        if (existingLogoImage.isPresent()) {
-            throw new AppException(ErrorCode.LOGO_IMAGE_EXIST);
-        }
-
-        Optional<ProjectImage> existingThumbnailImage = projectImageRepository.findThumbnailImageByProjectId(projectId);
-        if (existingThumbnailImage.isPresent()) {
-            throw new AppException(ErrorCode.THUMBNAIL_IMAGE_EXIST);
-        }
+        validateLogoImageExistence(projectId);
+        validateThumbNailImageExistence(projectId);
 
         List<ProjectImage> newImages = new ArrayList<>();
 
@@ -61,5 +54,17 @@ public class ProjectImageService {
         }
 
         projectImageRepository.saveAll(newImages);
+    }
+
+    private void validateThumbNailImageExistence(Long projectId) {
+        if (projectImageRepository.existsByProjectIdAndProjectImageType(projectId, ProjectImageType.THUMBNAIL)) {
+            throw new AppException(ErrorCode.THUMBNAIL_IMAGE_EXIST);
+        }
+    }
+
+    private void validateLogoImageExistence(Long projectId) {
+        if (projectImageRepository.existsByProjectIdAndProjectImageType(projectId, ProjectImageType.LOGO)) {
+            throw new AppException(ErrorCode.LOGO_IMAGE_EXIST);
+        }
     }
 }
