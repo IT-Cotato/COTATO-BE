138a17f2c3a75162686f0cd88a70588b5d627ce0
youth <boysoeng@naver.com>
2024-09-29 12:24:22 +0900

    chore: discord bot listener 설정

--- src/main/java/org/cotato/csquiz/common/config/JdaConfig.java
+++ src/main/java/org/cotato/csquiz/common/config/JdaConfig.java
@@ -1,19 +1,26 @@
 package org.cotato.csquiz.common.config;
 
+import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import org.cotato.csquiz.domain.education.service.DiscordButtonListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class JdaConfig {
+
+    private final DiscordButtonListener discordButtonListener;
 
     @Value("${discord.bot.token}")
     private String botToken;
 
     @Bean
     public JDA jda() throws InterruptedException {
-        return JDABuilder.createDefault(botToken).build().awaitReady();
+        return JDABuilder.createDefault(botToken)
+                .addEventListeners(discordButtonListener)
+                .build().awaitReady();
     }
 }
