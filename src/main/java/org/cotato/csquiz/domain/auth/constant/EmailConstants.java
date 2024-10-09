2f19fe39f8f3f0af26df084d3566e50f780b8372
gikhoon <ghikhoon@naver.com>
2024-10-09 14:29:20 +0900

    feat: 가입 거절 이메일 메세지 상수 추가

--- src/main/java/org/cotato/csquiz/domain/auth/constant/EmailConstants.java
+++ src/main/java/org/cotato/csquiz/domain/auth/constant/EmailConstants.java
@@ -22,7 +22,9 @@ public class EmailConstants {
     public static final String MEMBER_POSITION_PREFIX = "포지션: %s" + "<br>";
     public static final String MEMBER_GENERATION_PREFIX = "합격기수: %s" + "기<br>";
     public static final String SIGNUP_SUCCESS_SUBJECT = "코테이토 가입 승인이 완료됐습니다.";
+    public static final String SIGNUP_REJECT_SUBJECT = "코테이토 가입 승인이 거절됐습니다.";
     public static final String SIGNUP_SUCCESS_MESSAGE = "가입이 승인되었습니다<br>";
+    public static final String SIGNUP_FAIL_MESSAGE = "가입이 거절되었습니다<br>";
     public static final String SIGNUP_MESSAGE_PREFIX = ""
             + "<div style=\"background-color: #f2f2f2; padding: 20px;\">"
             + "<div style=\"background-color: #ffffff; padding: 20px;\">"
