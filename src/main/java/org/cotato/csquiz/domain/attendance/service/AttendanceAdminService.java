a90c425b56e46545de1029ef641520ea89a8cd6a
gikhoon <ghikhoon@naver.com>
2024-10-19 21:17:25 +0900

    feat: 출석 기록 수정 서비스 로직 구현

--- src/main/java/org/cotato/csquiz/domain/attendance/service/AttendanceAdminService.java
+++ src/main/java/org/cotato/csquiz/domain/attendance/service/AttendanceAdminService.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cotato.csquiz.api.attendance.dto.AttendanceDeadLineDto;
@@ -15,6 +17,9 @@ import org.cotato.csquiz.common.error.ErrorCode;
 import org.cotato.csquiz.common.error.exception.AppException;
 import org.cotato.csquiz.domain.attendance.embedded.Location;
 import org.cotato.csquiz.domain.attendance.entity.Attendance;
+import org.cotato.csquiz.domain.attendance.entity.AttendanceRecord;
+import org.cotato.csquiz.domain.attendance.enums.AttendanceRecordResult;
+import org.cotato.csquiz.domain.attendance.repository.AttendanceRecordRepository;
 import org.cotato.csquiz.domain.attendance.repository.AttendanceRepository;
 import org.cotato.csquiz.domain.attendance.util.AttendanceUtil;
 import org.cotato.csquiz.domain.generation.entity.Session;
@@ -29,6 +34,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class AttendanceAdminService {
 
     private final AttendanceRepository attendanceRepository;
+    private final AttendanceRecordRepository attendanceRecordRepository;
     private final AttendanceRecordService attendanceRecordService;
     private final SessionRepository sessionRepository;
 
@@ -75,7 +81,28 @@ public class AttendanceAdminService {
     }
 
     @Transactional
-    public void updateAttendanceRecords(Long attendanceId, List<UpdateAttendanceRecordInfoRequest> updateAttendanceRecordInfoRequests) {
+    public void updateAttendanceRecords(Long attendanceId,
+                                        List<UpdateAttendanceRecordInfoRequest> updateAttendanceRecordInfos) {
+        //멤버 아이디별 출석 기록 매핑
+        Map<Long, AttendanceRecord> recordMapByMemberId = attendanceRecordRepository.findAllByAttendanceId(attendanceId)
+                .stream()
+                .collect(Collectors.toMap(
+                        AttendanceRecord::getMemberId,
+                        record -> record
+                ));
+
+        Attendance attendance = attendanceRepository.findById(attendanceId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 출석이 존재하지 않습니다"));
+
+        updateAttendanceRecordInfos.forEach(updateRecordInfo -> {
+            AttendanceRecord memberAttendanceRecord = recordMapByMemberId.getOrDefault(
+                    updateRecordInfo.memberId(),
+                    AttendanceRecord.absentRecord(attendance, updateRecordInfo.memberId())
+            );
+
+            memberAttendanceRecord.updateByAttendanceRecordResult(updateRecordInfo.attendanceResult());
+            attendanceRecordRepository.save(memberAttendanceRecord);
+        });
     }
 
     public List<AttendanceRecordResponse> findAttendanceRecords(Long generationId, Integer month) {
