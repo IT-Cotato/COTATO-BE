5bd3fc9861e74d0726530760d761d9fa8fe3420c
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2024-10-19 16:54:54 +0900

    [COT-13] Feature: OM 전환 및 가입 승인 시 이메일로 결과 전송 (#173)
    * feat: 회원가입 성공 메시지 상수 추가
    
    * feat: 승인 완료 시 이메일 전송 로직 구현
    
    * feat: 가입 거절 이메일 메세지 상수 추가
    
    * feat: 승인 거절시 이메일 전송 로직 구현
    
    * feat: 승인 재승인 시 이메일 전송 로직 구현
    
    * fix: 메일 제목 상수 작성 오류 수정
    
    * chore: 가입 승인, 거부 로그 위치 변경
    
    * feat: OM 전환 이메일 메세지 상수 추가
    
    * feat: OM 전환 시 이메일 전송 로직 구현
    
    * refactor: 이메일 전송 실패시 커스텀 예외처리로 변경
    
    * refactor: 이메일 전송 성공 시 사용자 정보 반환 로직 합침
    
    * refactor: 메일 전송 로직 관련 메소드 명 변경
    
    * refactor: 메일본문 제작 로직 분리
    
    - 기존에 서비스 단에 있던 로직을 Util로 따로 관리하게 변경
    
    * refactor: 인증코드 메일본문 제작 로직 분리
    
    - 기존에 서비스 단에 있던 로직을 Util로 따로 관리하게 변경
    
    * refactor: 인증 코드 메일 전달 로직 리펙토링
    
    - 메일을 전달하는 코드를 sendMail로 통일
    
    * refactor: 이메일 전송 로직을 이메일 관련 서비스로 분리
    
    - 이메일 전송 기능과 관련된 디렉토리로 이동
    - EmailService => 이메일 관련 로직을 모아둔 클래스
    
    * refactor: 인증 결과 및 역할 변경 안내 클래스 분리
    
    - EmailNotificationService => 변경 안내 이메일 전송 관련 클래스

---
+++ src/main/java/org/cotato/csquiz/common/email/EmailService.java
@@ -0,0 +1,47 @@
+package org.cotato.csquiz.common.email;
+
+import static org.cotato.csquiz.domain.auth.constant.EmailConstants.SENDER_EMAIL;
+import static org.cotato.csquiz.domain.auth.constant.EmailConstants.SENDER_PERSONAL;
+
+import jakarta.mail.Message.RecipientType;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.cotato.csquiz.common.error.ErrorCode;
+import org.cotato.csquiz.common.error.exception.AppException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String recipient, String messageBody, String subject) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+
+            message.addRecipients(RecipientType.TO, recipient);
+            message.setSubject(subject);
+            message.setText(messageBody, "utf-8", "html");
+            message.setFrom(getInternetAddress());
+            mailSender.send(message);
+            log.info("이메일 전송 완료");
+        } catch (MessagingException e) {
+            throw new AppException(ErrorCode.EMAIL_SEND_ERROR);
+        }
+    }
+
+    private InternetAddress getInternetAddress() {
+        try {
+            return new InternetAddress(SENDER_EMAIL, SENDER_PERSONAL);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
