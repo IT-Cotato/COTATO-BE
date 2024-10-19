4b524af1acd95d53db190f61d08b83bde412f839
gikhoon <ghikhoon@naver.com>
2024-10-19 21:54:42 +0900

    feat: 출석 결석 기록 생성시 출석 시간 null로 변경
    - DDL 변경 진행 예정

--- src/main/java/org/cotato/csquiz/domain/attendance/entity/AttendanceRecord.java
+++ src/main/java/org/cotato/csquiz/domain/attendance/entity/AttendanceRecord.java
@@ -94,7 +94,7 @@ public class AttendanceRecord extends BaseTimeEntity {
             null,
             memberId,
             attendance,
-            LocalDateTime.now()
+            null
         );
     }
 
