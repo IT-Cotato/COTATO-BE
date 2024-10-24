ad2ede44f8b5afe505211934c8b0b49382dd9819
Youth <109585620+Youthhing@users.noreply.github.com>
2024-10-25 00:09:42 +0900

    [COT-27] Feature: 기수별 멤버 테이블 추가 (#184)
    * feat: create generation member table
    
    * chore: add member role description
    
    * refactor: memberrole fromKey to map

--- src/main/java/org/cotato/csquiz/domain/auth/enums/MemberRole.java
+++ src/main/java/org/cotato/csquiz/domain/auth/enums/MemberRole.java
@@ -1,31 +1,38 @@
 package org.cotato.csquiz.domain.auth.enums;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.cotato.csquiz.common.error.ErrorCode;
 import org.cotato.csquiz.common.error.exception.AppException;
 
 @Getter
+@RequiredArgsConstructor
 public enum MemberRole {
 
-    REFUSED("ROLE_REFUSED"),
-    GENERAL("ROLE_GENERAL"),
-    MEMBER("ROLE_MEMBER"),
-    OLD_MEMBER("ROLE_OM"),
-    ADMIN("ROLE_ADMIN"),
-    EDUCATION("ROLE_EDUCATION"),
-    OPERATION("ROLE_OPERATION");
+    REFUSED("ROLE_REFUSED", "승인 거절 부원"),
+    GENERAL("ROLE_GENERAL", "승인 대기 부원"),
+    MEMBER("ROLE_MEMBER", "현재 활동 중인 부원"),
+    OLD_MEMBER("ROLE_OM", "활동 후 종료한 부원"),
+    ADMIN("ROLE_ADMIN", "운영진"),
+    EDUCATION("ROLE_EDUCATION","교육팀"),
+    OPERATION("ROLE_OPERATION", "운영지원팀");
+
+    private static final Map<String, MemberRole> ROLE_KEY_MAP = Stream.of(values())
+            .collect(Collectors.toUnmodifiableMap(MemberRole::getKey, Function.identity()));
 
     private final String key;
-
-    MemberRole(String key) {
-        this.key = key;
-    }
+    private final String description;
 
     public static MemberRole fromKey(final String key) {
-        return Arrays.stream(MemberRole.values())
-                .filter(memberRole -> memberRole.getKey().equals(key))
-                .findFirst()
-                .orElseThrow(() -> new AppException(ErrorCode.ENUM_NOT_RESOLVED));
+        MemberRole result = ROLE_KEY_MAP.get(key);
+        if (result == null) {
+            throw new IllegalArgumentException(String.format("요청한 key(%s)를 찾을 수 없습니다.", key));
+        }
+        return result;
     }
 }
