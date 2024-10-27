3e4f38d44db8a4170112ad1aed3fd8f924f3bbd4
youth <boysoeng@naver.com>
2024-10-27 13:18:45 +0900

    feat: add data migration log table

---
+++ src/main/java/org/cotato/csquiz/migration/MigrationLogRepository.java
@@ -0,0 +1,8 @@
+package org.cotato.csquiz.migration;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MigrationLogRepository extends JpaRepository<MigrationLog, Long> {
+    Optional<MigrationLog> findByName(MigrationJob migrationJob);
+}
