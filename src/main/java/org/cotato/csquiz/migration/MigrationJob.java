e980d6e7609bd73dd14b5b5f3702f0acb499734f
Youth <109585620+Youthhing@users.noreply.github.com>
2024-10-27 14:26:59 +0900

    [COT-28] Feature: 현재 활동 기수 데이터 마이그레이션 (#185)
    * chore: add lombok to test
    
    * feat: add data migration log table
    
    * feat: add data migration runner
    
    * feat: add current generation migration runner
    
    * fix: change jpa method parameter
    
    * refactor: change to stream

---
+++ src/main/java/org/cotato/csquiz/migration/MigrationJob.java
@@ -0,0 +1,10 @@
+package org.cotato.csquiz.migration;
+
+public interface MigrationJob {
+
+    void migrate();
+
+    default String getName(){
+        return getClass().getSimpleName();
+    }
+}
