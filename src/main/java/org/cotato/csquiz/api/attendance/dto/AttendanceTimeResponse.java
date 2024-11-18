ea2488191e690235e8d3da3bed4ea970c9b37d49
gikhoon <ghikhoon@naver.com>
2024-11-18 23:30:53 +0900

    feat: change type of attendanceTime
    LocalTime -> LocalDateTime

--- src/main/java/org/cotato/csquiz/api/attendance/dto/AttendanceTimeResponse.java
+++ src/main/java/org/cotato/csquiz/api/attendance/dto/AttendanceTimeResponse.java
@@ -1,21 +1,21 @@
 package org.cotato.csquiz.api.attendance.dto;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import org.cotato.csquiz.domain.attendance.embedded.Location;
 import org.cotato.csquiz.domain.attendance.entity.Attendance;
 
 public record AttendanceTimeResponse(
         Long sessionId,
-        LocalTime attendanceDeadLine,
-        LocalTime lateDeadLine,
+        LocalDateTime attendanceDeadLine,
+        LocalDateTime lateDeadLine,
         Location location
 ) {
 
     public static AttendanceTimeResponse from(Attendance attendance) {
         return new AttendanceTimeResponse(
                 attendance.getSessionId(),
-                attendance.getAttendanceDeadLine().toLocalTime(),
-                attendance.getLateDeadLine().toLocalTime(),
+                attendance.getAttendanceDeadLine(),
+                attendance.getLateDeadLine(),
                 attendance.getLocation()
         );
     }
