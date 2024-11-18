0ef3c91a290cc591143d7577df284065e0fcde7a
gikhoon <ghikhoon@naver.com>
2024-11-19 01:01:33 +0900

    feat: add requiredMode schema of attendanceTimeResponse

--- src/main/java/org/cotato/csquiz/api/attendance/dto/AttendanceTimeResponse.java
+++ src/main/java/org/cotato/csquiz/api/attendance/dto/AttendanceTimeResponse.java
@@ -1,12 +1,15 @@
 package org.cotato.csquiz.api.attendance.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import org.cotato.csquiz.domain.attendance.embedded.Location;
 import org.cotato.csquiz.domain.attendance.entity.Attendance;
 
 public record AttendanceTimeResponse(
         Long sessionId,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
         LocalDateTime attendanceDeadLine,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
         LocalDateTime lateDeadLine,
         Location location
 ) {
