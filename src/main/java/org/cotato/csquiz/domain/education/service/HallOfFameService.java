3b9f9d4991da7c04dc2fd33a76b411229bd315f2
gikhoon <ghikhoon@naver.com>
2024-11-22 01:12:48 +0900

    refactor: rename hall of fame service
    -> myPageService -> HallOfFameService

--- src/main/java/org/cotato/csquiz/domain/education/service/HallOfFameService.java
+++ src/main/java/org/cotato/csquiz/domain/education/service/HallOfFameService.java
@@ -31,7 +31,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Slf4j
-public class MyPageService {
+public class HallOfFameService {
 
     private static final int SHOW_PEOPLE_COUNT = 5;
     private final MemberService memberService;
