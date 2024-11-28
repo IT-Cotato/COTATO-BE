67129a016cbcce328861a3f3a15ed987b34c8dad
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2024-11-29 01:28:15 +0900

    [COT-72] Feature: 프로필 링크 관련 테이블 추가 (#209)
    * feat: add university, introduction column in member entity
    
    * feat: create profile link entity
    
    * refactor: rename hall of fame service
    
    -> myPageService -> HallOfFameService
    
    * feat: create memberProfile service, repository file
    
    * feat: delete limit of introduction, university column length
    
    * refactor: rollback name of hall of fame service
    
    * feat: add TEXT option in introduction
    
    * refactor: change parameter of hall of fame API
    
    -> accessHeader -> AuthenticationPrincipal
    
    * refactor: delete unused Builder from ProfileLink

--- src/main/java/org/cotato/csquiz/api/mypage/controller/MyPageController.java
+++ src/main/java/org/cotato/csquiz/api/mypage/controller/MyPageController.java
@@ -1,13 +1,12 @@
 package org.cotato.csquiz.api.mypage.controller;
 
 import org.cotato.csquiz.api.mypage.dto.HallOfFameResponse;
-import org.cotato.csquiz.common.config.jwt.JwtTokenProvider;
 import org.cotato.csquiz.domain.education.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyPageController {
 
     private final MyPageService myPageService;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/hall-of-fame")
     public ResponseEntity<HallOfFameResponse> findHallOfFame(@RequestParam("generationId") Long generationId,
-                                                             @RequestHeader("Authorization") String authorizationHeader) {
-        String accessToken = jwtTokenProvider.getBearer(authorizationHeader);
-
-        return ResponseEntity.ok(myPageService.findHallOfFame(generationId, jwtTokenProvider.getMemberId(accessToken)));
+                                                             @AuthenticationPrincipal Long memberId) {
+        return ResponseEntity.ok(myPageService.findHallOfFame(generationId, memberId));
     }
 }
---
+++ src/main/java/org/cotato/csquiz/domain/auth/enums/LinkType.java
@@ -0,0 +1,16 @@
+package org.cotato.csquiz.domain.auth.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum LinkType {
+    GITHUB("깃허브"),
+    BEHANCE("비핸스"),
+    BLOG("블로그"),
+    OTHER("기타"),
+    ;
+
+    private final String description;
+}
