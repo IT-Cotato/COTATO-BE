67b4e3b2774d01a19ebde3c313814f4a041d57a5
gikhoon <ghikhoon@naver.com>
2024-11-30 15:19:42 +0900

    refactor: delete unused import of GenerationMember Entity

--- src/main/java/org/cotato/csquiz/domain/generation/entity/GenerationMember.java
+++ src/main/java/org/cotato/csquiz/domain/generation/entity/GenerationMember.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.cotato.csquiz.common.entity.BaseTimeEntity;
