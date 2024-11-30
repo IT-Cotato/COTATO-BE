0b190c6cca21974d22184bd6f914e748d124ef68
gikhoon <ghikhoon@naver.com>
2024-11-30 23:09:00 +0900

    refactor: delete unused Annotation

--- src/main/java/org/cotato/csquiz/api/member/controller/GenerationMemberController.java
+++ src/main/java/org/cotato/csquiz/api/member/controller/GenerationMemberController.java
@@ -3,7 +3,6 @@ package org.cotato.csquiz.api.member.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.cotato.csquiz.api.member.dto.CreateGenerationMemberRequest;
 import org.cotato.csquiz.api.member.dto.UpdateGenerationMemberRoleRequest;
 import org.cotato.csquiz.domain.auth.service.GenerationMemberService;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "기수별 활동 멤버 관련 API")
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v2/api/generation-member")
