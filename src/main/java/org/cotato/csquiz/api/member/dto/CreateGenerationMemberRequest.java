f9ee6a4362d5890b4107bf4671d02a1c9b9b48a9
gikhoon <ghikhoon@naver.com>
2024-11-30 22:28:42 +0900

    style: Remove unnecessary line breaks for code convention

--- src/main/java/org/cotato/csquiz/api/member/dto/CreateGenerationMemberRequest.java
+++ src/main/java/org/cotato/csquiz/api/member/dto/CreateGenerationMemberRequest.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotNull;
 public record CreateGenerationMemberRequest(
         @NotNull(message = "멤버 아이디를 입력해주세요")
         Long memberId,
-
         @NotNull(message = "기수를 입력해주세요")
         Long generationId
 ) {
