d71697802b92f1b6697bbb00f05d889d8d9fb7a2
Youth <109585620+Youthhing@users.noreply.github.com>
2024-12-11 23:19:59 +0900

    [COT-113] Feature: 세션 단건 조회 시 출결 필드 반환 (#239)
    * feat: add session type field
    
    * chore: add required description

--- src/main/java/org/cotato/csquiz/api/session/dto/SessionWithAttendanceResponse.java
+++ src/main/java/org/cotato/csquiz/api/session/dto/SessionWithAttendanceResponse.java
@@ -1,5 +1,6 @@
 package org.cotato.csquiz.api.session.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.cotato.csquiz.api.attendance.dto.AttendanceTimeResponse;
@@ -9,15 +10,20 @@ import org.cotato.csquiz.domain.generation.entity.Session;
 import org.cotato.csquiz.domain.generation.entity.SessionImage;
 
 public record SessionWithAttendanceResponse(
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
         Long sessionId,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
         Integer sessionNumber,
         String title,
         List<SessionListImageInfoResponse> sessionImages,
         String description,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
         Long generationId,
         String placeName,
         LocalDateTime sessionDateTime,
         SessionContents sessionContents,
+        boolean isOffline,
+        boolean isOnline,
         AttendanceTimeResponse attendance
 ) {
     public static SessionWithAttendanceResponse of(Session session, List<SessionImage> sessionImages, Attendance attendance) {
@@ -31,6 +37,8 @@ public record SessionWithAttendanceResponse(
             session.getPlaceName(),
             session.getSessionDateTime(),
             session.getSessionContents(),
+            session.getSessionType().hasOffline(),
+            session.getSessionType().hasOnline(),
             AttendanceTimeResponse.from(attendance)
         );
     }
@@ -46,6 +54,8 @@ public record SessionWithAttendanceResponse(
                 session.getPlaceName(),
                 session.getSessionDateTime(),
                 session.getSessionContents(),
+                session.getSessionType().hasOffline(),
+                session.getSessionType().hasOnline(),
                 null
         );
     }
