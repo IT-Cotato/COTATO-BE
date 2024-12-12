e54c7a020c0bc5afcc22d9b1f1e27bb866ca59b7
gikhoon <ghikhoon@naver.com>
2024-12-12 17:20:01 +0900

    Revert "feat: add validation of attendance time while updating session"
    This reverts commit 6fad4dfbc6e227052826d9d11b13dd9de557ea09.

--- src/main/java/org/cotato/csquiz/domain/generation/service/SessionService.java
+++ src/main/java/org/cotato/csquiz/domain/generation/service/SessionService.java
@@ -20,7 +20,6 @@ import org.cotato.csquiz.domain.attendance.entity.Attendance;
 import org.cotato.csquiz.domain.attendance.repository.AttendanceRepository;
 import org.cotato.csquiz.domain.attendance.service.AttendanceService;
 import org.cotato.csquiz.domain.attendance.service.component.AttendanceReader;
-import org.cotato.csquiz.domain.attendance.util.AttendanceUtil;
 import org.cotato.csquiz.domain.education.entity.Education;
 import org.cotato.csquiz.domain.education.service.EducationService;
 import org.cotato.csquiz.domain.generation.embedded.SessionContents;
@@ -117,8 +116,6 @@ public class SessionService {
         sessionRepository.save(session);
 
         // Todo https://www.notion.so/youthhing/ApplicationEventPublisher-15887d592b6e803eb7c7c1ce2da22b8c?pvs=4
-        AttendanceUtil.validateAttendanceTime(request.sessionDateTime(), request.attendTime().attendanceDeadLine(),
-                request.attendTime().lateDeadLine());
         Attendance attendance = attendanceReader.findBySessionIdWithPessimisticXLock(session.getId())
                 .orElseGet(() -> Attendance.builder()
                         .session(session)
