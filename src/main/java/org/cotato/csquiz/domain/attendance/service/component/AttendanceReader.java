a808c9b9efcb9821724ebf862888a9e3db01dcaf
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2024-12-15 00:01:06 +0900

    [COT-89] Feature: 세션 수정 시 비관적 X-Lock 생성 (#243)
    * feat: add pessimistic x-lock while finding session by id
    
    * feat: add pessimistic x-lock while finding attendance by sessionId
    
    * feat: add validation of attendance time while updating session
    
    * Revert "feat: add validation of attendance time while updating session"
    
    This reverts commit 6fad4dfbc6e227052826d9d11b13dd9de557ea09.
    
    * style: remove unnecessary line
    
    * feat: add read only transaction in select jpql

--- src/main/java/org/cotato/csquiz/domain/attendance/service/component/AttendanceReader.java
+++ src/main/java/org/cotato/csquiz/domain/attendance/service/component/AttendanceReader.java
@@ -1,6 +1,7 @@
 package org.cotato.csquiz.domain.attendance.service.component;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.cotato.csquiz.domain.attendance.entity.Attendance;
 import org.cotato.csquiz.domain.attendance.repository.AttendanceRepository;
@@ -14,5 +15,9 @@ public class AttendanceReader {
 
     public Attendance findById(final Long id) {
         return attendanceRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 출석 정보를 찾을 수 없습니다."));
+    }
+
+    public Optional<Attendance> findBySessionIdWithPessimisticXLock(Long sessionId) {
+        return attendanceRepository.findBySessionIdWithPessimisticXLock(sessionId);
     }
 }
