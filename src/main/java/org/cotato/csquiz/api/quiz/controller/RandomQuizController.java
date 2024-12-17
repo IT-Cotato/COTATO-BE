777948d15b50b94c4999919a33358ba27205fccb
gikhoon <ghikhoon@naver.com>
2024-12-17 17:31:53 +0900

    feat: develop reply to randomQuiz API

--- src/main/java/org/cotato/csquiz/api/quiz/controller/RandomQuizController.java
+++ src/main/java/org/cotato/csquiz/api/quiz/controller/RandomQuizController.java
@@ -2,11 +2,17 @@ package org.cotato.csquiz.api.quiz.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.cotato.csquiz.api.quiz.dto.RandomQuizReplyRequest;
+import org.cotato.csquiz.api.quiz.dto.RandomQuizReplyResponse;
 import org.cotato.csquiz.api.quiz.dto.RandomTutorialQuizResponse;
 import org.cotato.csquiz.domain.education.service.RandomQuizService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +28,12 @@ public class RandomQuizController {
     @GetMapping
     public ResponseEntity<RandomTutorialQuizResponse> pickRandomQuiz() {
         return ResponseEntity.ok().body(randomQuizService.pickRandomQuiz());
+    }
+
+    @Operation(summary = "외부인용 랜덤 퀴즈 제출 API")
+    @PostMapping("/{quizId}/reply")
+    public ResponseEntity<RandomQuizReplyResponse> replyToRandomQuiz(@PathVariable(name = "quizId") final Long quizId,
+                                                                     @Valid @RequestBody RandomQuizReplyRequest request) {
+        return ResponseEntity.ok().body(randomQuizService.replyToRandomQuiz(quizId, request.input()));
     }
 }
