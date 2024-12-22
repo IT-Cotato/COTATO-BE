e67713159eb2e73c51ab8d74156c4b490c0cc238
gikhoon <ghikhoon@naver.com>
2024-12-22 16:29:39 +0900

    Revert "refactor: change type of quiz result"
    This reverts commit fe1432338b78f6674b7d923e4eea6fd56c4952e7.

--- src/main/java/org/cotato/csquiz/api/record/dto/ReplyResponse.java
+++ src/main/java/org/cotato/csquiz/api/record/dto/ReplyResponse.java
@@ -1,14 +1,10 @@
 package org.cotato.csquiz.api.record.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
-
 public record ReplyResponse(
-        @Schema(requiredMode = RequiredMode.REQUIRED)
-        boolean result
+        String result
 ) {
 
     public static ReplyResponse from(Boolean isCorrect) {
-        return new ReplyResponse(isCorrect);
+        return new ReplyResponse(isCorrect.toString());
     }
 }
