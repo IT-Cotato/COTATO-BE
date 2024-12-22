fe1432338b78f6674b7d923e4eea6fd56c4952e7
gikhoon <ghikhoon@naver.com>
2024-12-22 16:10:15 +0900

    refactor: change type of quiz result

--- src/main/java/org/cotato/csquiz/api/record/dto/ReplyResponse.java
+++ src/main/java/org/cotato/csquiz/api/record/dto/ReplyResponse.java
@@ -1,10 +1,14 @@
 package org.cotato.csquiz.api.record.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+
 public record ReplyResponse(
-        String result
+        @Schema(requiredMode = RequiredMode.REQUIRED)
+        boolean result
 ) {
 
     public static ReplyResponse from(Boolean isCorrect) {
-        return new ReplyResponse(isCorrect.toString());
+        return new ReplyResponse(isCorrect);
     }
 }
