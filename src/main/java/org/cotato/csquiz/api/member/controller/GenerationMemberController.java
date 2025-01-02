7a424e3c2dc2bb8e35a6e273e55cb8c5ac7d1730
youth <boysoeng@naver.com>
2025-01-03 00:53:32 +0900

    feat: generation-member api 권한 설정

--- src/main/java/org/cotato/csquiz/api/member/controller/GenerationMemberController.java
+++ src/main/java/org/cotato/csquiz/api/member/controller/GenerationMemberController.java
@@ -7,6 +7,8 @@ import org.cotato.csquiz.api.member.dto.CreateGenerationMemberRequest;
 import org.cotato.csquiz.api.member.dto.DeleteGenerationMemberRequest;
 import org.cotato.csquiz.api.member.dto.GenerationMemberInfoResponse;
 import org.cotato.csquiz.api.member.dto.UpdateGenerationMemberRoleRequest;
+import org.cotato.csquiz.common.role.RoleAuthority;
+import org.cotato.csquiz.domain.auth.enums.MemberRole;
 import org.cotato.csquiz.domain.auth.service.GenerationMemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,23 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "기수별 활동 멤버 관련 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v2/api/generation-member")
+@RequestMapping("/v2/api/generation-members")
 public class GenerationMemberController {
 
     private final GenerationMemberService generationMemberService;
 
+    @RoleAuthority(MemberRole.ADMIN)
     @GetMapping
     public ResponseEntity<GenerationMemberInfoResponse> findGenerationMember(
             @RequestParam("generationId") Long generationId) {
         return ResponseEntity.ok().body(generationMemberService.findGenerationMemberByGeneration(generationId));
     }
 
+    @RoleAuthority(MemberRole.ADMIN)
     @PostMapping
     public ResponseEntity<Void> addGenerationMember(@RequestBody @Valid CreateGenerationMemberRequest request) {
         generationMemberService.addGenerationMember(request.generationId(), request.memberIds());
         return ResponseEntity.noContent().build();
     }
 
+    @RoleAuthority(MemberRole.ADMIN)
     @PatchMapping
     public ResponseEntity<Void> updateGenerationMemberRole(
             @RequestBody @Valid UpdateGenerationMemberRoleRequest request) {
@@ -45,6 +50,7 @@ public class GenerationMemberController {
         return ResponseEntity.noContent().build();
     }
 
+    @RoleAuthority(MemberRole.ADMIN)
     @DeleteMapping
     public ResponseEntity<Void> deleteGenerationMember(@RequestBody @Valid DeleteGenerationMemberRequest request) {
         generationMemberService.deleteGenerationMembers(request.generationMemberIds());
