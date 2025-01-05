93d92541315c0f3731a7746a2edcb665c2738a91
Youth <109585620+Youthhing@users.noreply.github.com>
2025-01-05 23:39:26 +0900

    [COT-130] Feature: 어노테이션을 통한 권한에 따른 API 접근 설정 (#257)
    * feat: 어노테이션을 통한 권한에 따른 API 접근 설정
    
    * feat: add priority field in member role

---
+++ src/main/java/org/cotato/csquiz/common/role/RoleAuthority.java
@@ -0,0 +1,13 @@
+package org.cotato.csquiz.common.role;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.cotato.csquiz.domain.auth.enums.MemberRole;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RoleAuthority {
+    MemberRole value();
+}
