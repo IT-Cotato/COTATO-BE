2ac1d69ebc21925d9d5c4d94fcf3400901cce5db
Youth <109585620+Youthhing@users.noreply.github.com>
2025-01-05 23:39:26 +0900

    [COT-130] Feature: 어노테이션을 통한 권한에 따른 API 접근 설정 (#257)
    * feat: 어노테이션을 통한 권한에 따른 API 접근 설정
    
    * feat: add priority field in member role

--- src/main/java/org/cotato/csquiz/common/config/WebConfig.java
+++ src/main/java/org/cotato/csquiz/common/config/WebConfig.java
@@ -1,8 +1,8 @@
 package org.cotato.csquiz.common.config;
 
 import lombok.RequiredArgsConstructor;
-import org.cotato.csquiz.common.idempotency.IdempotencyInterceptor;
 import org.cotato.csquiz.common.idempotency.IdempotencyRedisRepository;
+import org.cotato.csquiz.common.role.RoleInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,6 +15,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RoleInterceptor())
+                .addPathPatterns("/**")
+                .order(1);
 //        registry.addInterceptor(new IdempotencyInterceptor(idempotencyRedisRepository))
 //                .addPathPatterns("/v1/api/record/reply")
 //                .order(1);
