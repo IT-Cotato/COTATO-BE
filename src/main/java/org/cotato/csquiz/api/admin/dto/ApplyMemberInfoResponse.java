7465b6651a6f50ed6afd7bfa19a14c52c7500cfb
Youth <109585620+Youthhing@users.noreply.github.com>
2025-01-07 23:48:43 +0900

    [COT-134] Feature: MemberStatus 필드 추가 (#259)
    * feat: add member status field
    
    * feat: 회원 가입 시 기본 상태 설정
    
    * feat: admin service 작동 수정
    
    * fix: set default value explicitly
    
    * feat: 거절된 부원 제거 스케줄러 로직 변경
    
    * test: builder 로 인해 깨지는 테스트 수정

--- src/main/java/org/cotato/csquiz/api/admin/dto/ApplyMemberInfoResponse.java
+++ src/main/java/org/cotato/csquiz/api/admin/dto/ApplyMemberInfoResponse.java
@@ -1,10 +1,15 @@
 package org.cotato.csquiz.api.admin.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import org.cotato.csquiz.domain.auth.entity.Member;
 
 public record ApplyMemberInfoResponse(
+        @Schema(requiredMode = RequiredMode.REQUIRED)
         Long id,
+        @Schema(requiredMode = RequiredMode.REQUIRED)
         String name,
+        @Schema(requiredMode = RequiredMode.REQUIRED)
         String backFourNumber
 ) {
 
