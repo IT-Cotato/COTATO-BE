e12f208d60674a5c609c06668e9f5de8e85cfa92
youth <boysoeng@naver.com>
2025-01-08 14:29:13 +0900

    feat: 기수에 해당하는 부원 여부 확인 Validator 구현

---
+++ src/main/java/org/cotato/csquiz/domain/auth/component/GenerationMemberAuthValidator.java
@@ -0,0 +1,30 @@
+package org.cotato.csquiz.domain.auth.component;
+
+import lombok.RequiredArgsConstructor;
+import org.cotato.csquiz.common.error.ErrorCode;
+import org.cotato.csquiz.common.error.exception.AppException;
+import org.cotato.csquiz.domain.auth.entity.Member;
+import org.cotato.csquiz.domain.generation.entity.Generation;
+import org.cotato.csquiz.domain.generation.repository.GenerationMemberRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GenerationMemberAuthValidator {
+
+    private final GenerationMemberRepository generationMemberRepository;
+
+    public void checkGenerationPermission(final Member member, final Generation generation) {
+        if (member.isDevTeam()) {
+            return;
+        }
+
+        checkIsGenerationMember(member, generation);
+    }
+
+    private void checkIsGenerationMember(Member member, Generation generation) {
+        if (!generationMemberRepository.existsByGenerationAndMember(generation, member)) {
+            throw new AppException(ErrorCode.ATTENDANCE_PERMISSION);
+        }
+    }
+}
