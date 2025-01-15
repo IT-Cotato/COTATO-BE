7f0a312072437b4b421cfaea0e4beafe462d3598
youth <boysoeng@naver.com>
2025-01-15 17:48:56 +0900

    chore: 사용하지 않는 이메일 검증 구현체 삭제

--- src/main/java/org/cotato/csquiz/common/error/ErrorCode.java
+++ src/main/java/org/cotato/csquiz/common/error/ErrorCode.java
@@ -24,7 +24,6 @@ public enum ErrorCode {
     ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "I-201", "해당 Entity를 찾을 수 없습니다."),
 
     // 회원 가입
-    EMAIL_TYPE_ERROR(HttpStatus.BAD_REQUEST, "A-001", "구글, 네이버 형식으로 입력해주세요"),
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "A-002", "유효하지 않은 패스워드입니다."),
     INVALID_PHONE_NUMBER(HttpStatus.BAD_REQUEST, "A-003", "유효하지 않은 전화번호 입니다."),
     CODE_NOT_MATCH(HttpStatus.BAD_REQUEST, "A-101", "요청하신 코드가 일치하지 않습니다."),
