ce015bc9fd9240a07cb47402a1f2f80b19d95de7
Youth <109585620+Youthhing@users.noreply.github.com>
2025-01-15 22:30:23 +0900

    [COT-151] Feature: 지원하는 이메일 형식을 제한하지 않는다. (#265)
    * feat: email verification service 네이밍 변경
    
    * feat: 회원가입 및 비밀번호 찿기 이메일 발송 로직 수정
    
    * feat: 역할 변경 시 발송하는 이메일 검증 로직 제거
    
    * chore: 사용하지 않는 이메일 검증 구현체 삭제
    
    * feat: 불필요한 어노테이션 제거 및 계층 변경

--- src/main/java/org/cotato/csquiz/domain/auth/service/component/EmailCodeManager.java
+++ src/main/java/org/cotato/csquiz/domain/auth/service/component/EmailCodeManager.java
@@ -1,49 +1,34 @@
-package org.cotato.csquiz.domain.auth.service;
-
-import static org.cotato.csquiz.common.util.EmailUtil.getVerificationMessageBody;
+package org.cotato.csquiz.domain.auth.service.component;
 
 import java.util.concurrent.ThreadLocalRandom;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.cotato.csquiz.common.email.EmailService;
-import org.cotato.csquiz.common.error.exception.AppException;
 import org.cotato.csquiz.common.error.ErrorCode;
+import org.cotato.csquiz.common.error.exception.AppException;
 import org.cotato.csquiz.domain.auth.cache.EmailRedisRepository;
-import org.cotato.csquiz.domain.auth.enums.EmailType;
-import org.cotato.csquiz.domain.auth.utils.EmailFormValidator;
 import org.cotato.csquiz.domain.auth.cache.VerificationCodeRedisRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.cotato.csquiz.domain.auth.enums.EmailType;
+import org.springframework.stereotype.Component;
 
 @Slf4j
-@Service
+@Component
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class EmailVerificationService {
+public class EmailCodeManager {
 
     private static final int CODE_LENGTH = 6;
     private static final int CODE_BOUNDARY = 10;
-
-    private final EmailService emailService;
     private final VerificationCodeRedisRepository verificationCodeRedisRepository;
-    private final EmailFormValidator emailFormValidator;
     private final EmailRedisRepository emailRedisRepository;
 
-    public void sendVerificationCodeToEmail(EmailType type, String recipient, String subject) {
-        emailFormValidator.validateEmailForm(recipient);
-
-        String verificationCode = getVerificationCode();
-        log.info("인증 번호 생성 완료");
-
+    public String getRandomCode(final EmailType type, final String recipient) {
+        String verificationCode = getRandomCode();
         emailRedisRepository.saveEmail(type, recipient);
         verificationCodeRedisRepository.saveCodeWithEmail(type, recipient, verificationCode);
 
-        String verificationMessage = getVerificationMessageBody(verificationCode);
-
-        emailService.sendEmail(recipient, verificationMessage, subject);
+        return verificationCode;
     }
 
-    private String getVerificationCode() {
+    private String getRandomCode() {
         final ThreadLocalRandom random = ThreadLocalRandom.current();
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < CODE_LENGTH; i++) {
