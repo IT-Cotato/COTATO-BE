1a5c0c0e0c06f6d880f351e27695062131a9b666
youth <boysoeng@naver.com>
2025-01-15 17:56:08 +0900

    feat: 전화번호 검증 에러 형식 구분

--- src/main/java/org/cotato/csquiz/domain/auth/service/ValidateService.java
+++ src/main/java/org/cotato/csquiz/domain/auth/service/ValidateService.java
@@ -54,19 +54,15 @@ public class ValidateService {
     }
 
     public void checkPhoneNumber(String phoneNumber) {
-        if (phoneNumber.length() != PHONE_NUMBER_LENGTH) {
-            log.error("[전화번호 에러]: 적당하지 않은 길이");
-            throw new AppException(ErrorCode.INVALID_PHONE_NUMBER);
-        }
         if (!phoneNumber.startsWith(PHONE_NUMBER_PREFIX)) {
             log.error("[전화번호 에러]: 010으로 시작하지 않음");
-            throw new AppException(ErrorCode.INVALID_PHONE_NUMBER);
+            throw new AppException(ErrorCode.INVALID_PHONE_NUMBER_PREFIX);
         }
         try {
             Integer.parseInt(phoneNumber);
         } catch (Exception e) {
             log.error("[전화번호 에러]: 문자열 파싱 에러");
-            throw new AppException(ErrorCode.INVALID_PHONE_NUMBER);
+            throw new AppException(ErrorCode.INVALID_PHONE_NUMBER_FORMAT);
         }
     }
 }
