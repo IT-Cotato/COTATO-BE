e54b12abbc438cac126bff6f55bb000af184c228
Youth <109585620+Youthhing@users.noreply.github.com>
2025-01-15 22:30:23 +0900

    [COT-151] Feature: 지원하는 이메일 형식을 제한하지 않는다. (#265)
    * feat: email verification service 네이밍 변경
    
    * feat: 회원가입 및 비밀번호 찿기 이메일 발송 로직 수정
    
    * feat: 역할 변경 시 발송하는 이메일 검증 로직 제거
    
    * chore: 사용하지 않는 이메일 검증 구현체 삭제
    
    * feat: 불필요한 어노테이션 제거 및 계층 변경

--- src/main/java/org/cotato/csquiz/domain/auth/service/EmailNotificationService.java
+++ src/main/java/org/cotato/csquiz/domain/auth/service/EmailNotificationService.java
@@ -9,9 +9,8 @@ import static org.cotato.csquiz.domain.auth.constant.EmailConstants.SIGNUP_SUCCE
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.cotato.csquiz.common.email.EmailService;
+import org.cotato.csquiz.common.email.EmailSender;
 import org.cotato.csquiz.domain.auth.entity.Member;
-import org.cotato.csquiz.domain.auth.utils.EmailFormValidator;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -19,33 +18,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailNotificationService {
 
-    private final EmailService emailService;
-    private final EmailFormValidator emailFormValidator;
+    private final EmailSender emailSender;
 
     public void sendSignUpApprovedToEmail(Member recipientMember) {
-        emailFormValidator.validateEmailForm(recipientMember.getEmail());
-
         String successMessage = createSignupApprovedMessageBody(recipientMember);
 
-        emailService.sendEmail(recipientMember.getEmail(), successMessage, SIGNUP_SUCCESS_SUBJECT);
+        emailSender.sendEmail(recipientMember.getEmail(), successMessage, SIGNUP_SUCCESS_SUBJECT);
         log.info("가입 승인 완료 이메일 전송 완료");
     }
 
     public void sendSignupRejectionToEmail(Member recipientMember) {
-        emailFormValidator.validateEmailForm(recipientMember.getEmail());
-
         String rejectMessage = createSignupRejectionMessageBody(recipientMember);
 
-        emailService.sendEmail(recipientMember.getEmail(), rejectMessage, SIGNUP_REJECT_SUBJECT);
+        emailSender.sendEmail(recipientMember.getEmail(), rejectMessage, SIGNUP_REJECT_SUBJECT);
         log.info("가입 승인 거절 이메일 전송 완료");
     }
 
     public void sendOldMemberConversionToEmail(Member recipientMember) {
-        emailFormValidator.validateEmailForm(recipientMember.getEmail());
-
         String conversionMessageBody = createOldMemberConversionEmailBody(recipientMember);
 
-        emailService.sendEmail(recipientMember.getEmail(), conversionMessageBody, CONVERSION_TO_OM_SUBJECT);
+        emailSender.sendEmail(recipientMember.getEmail(), conversionMessageBody, CONVERSION_TO_OM_SUBJECT);
         log.info("OM 전환 이메일 전송 완료");
     }
 }
