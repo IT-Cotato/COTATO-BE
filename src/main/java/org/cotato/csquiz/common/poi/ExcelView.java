5519fe2a3e1318ddbe4d2518ef2a2008c4753750
youth <boysoeng@naver.com>
2025-01-16 15:50:05 +0900

    feat: ModelAndView를 활용한 엑셀 다운로드 세팅

---
+++ src/main/java/org/cotato/csquiz/common/poi/ExcelView.java
@@ -0,0 +1,20 @@
+package org.cotato.csquiz.common.poi;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+@Component
+@RequiredArgsConstructor
+public class ExcelView extends AbstractXlsxView {
+
+    @Override
+    protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
+                                      HttpServletResponse response) {
+        new ExcelWriter(model, response).create();
+    }
+}
---
+++ src/main/java/org/cotato/csquiz/common/poi/ExcelWriter.java
@@ -0,0 +1,74 @@
+package org.cotato.csquiz.common.poi;
+
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+@RequiredArgsConstructor
+public class ExcelWriter {
+
+    private static final String FILE_NAME = "fileName";
+    private static final String SHEETS = "sheets";
+    private final Map<String, Object> data;
+    private final HttpServletResponse response;
+
+    @SuppressWarnings("unchecked")
+    public void create() {
+        Workbook workbook = new XSSFWorkbook();
+        setFileName(response, (String) data.get(FILE_NAME));
+
+        Map<String, ?> dataBySheet = (Map<String, ?>) data.get(SHEETS);
+
+        for (Entry<String, ?> sheetAndDatas : dataBySheet.entrySet()) {
+            String sheetName = sheetAndDatas.getKey();
+            Sheet sheet = workbook.createSheet(sheetName);
+            createHeaderRow(sheet, sheetAndDatas.getValue());
+            createDataRow(sheet, sheetAndDatas.getValue());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void createHeaderRow(Sheet sheet, Object datas) {
+        Row headerRow = sheet.createRow(0);
+        List<ExcelData> excelData = (List<ExcelData>) datas;
+        int cellNumber = 0;
+        ExcelData cellData = excelData.get(0);
+        List<CellData> headers = cellData.headers();
+
+        for (CellData header : headers) {
+            Cell cell = headerRow.createCell(cellNumber++);
+            cell.setCellValue(header.getValue());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void createDataRow(Sheet sheet, Object datas) {
+        List<ExcelData> excelData = (List<ExcelData>) datas;
+
+        int rowNumber = 1;
+        for (ExcelData columnValue : excelData) {
+            Row row = sheet.createRow(rowNumber++);
+            createData(row, columnValue.datas());
+        }
+    }
+
+    private void createData(Row row, List<CellData> datas) {
+        int columnNumber = 0;
+        for (CellData cellData : datas) {
+            Cell cell = row.createCell(columnNumber++);
+            cell.setCellValue(cellData.getValue());
+        }
+    }
+
+    private void setFileName(HttpServletResponse response, String fileName) {
+        response.setHeader("Content-Disposition",
+                "attachment; filename=\"" + fileName + ".xlsx" + "\"");
+    }
+}
