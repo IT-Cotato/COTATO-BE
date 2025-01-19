7dd0d56ac58542d20942039e970dd347ab5e35b2
youth <boysoeng@naver.com>
2025-01-20 02:43:25 +0900

    feat: 기수별 출결 기록 조회 API 경로 수정

--- src/main/java/org/cotato/csquiz/api/attendance/controller/AttendanceExcelController.java
+++ src/main/java/org/cotato/csquiz/api/attendance/controller/AttendanceExcelController.java
@@ -1,11 +1,7 @@
 package org.cotato.csquiz.api.attendance.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.cotato.csquiz.common.error.ErrorCode;
-import org.cotato.csquiz.common.error.exception.AppException;
-import org.cotato.csquiz.common.poi.AttendanceExcelSheetType;
 import org.cotato.csquiz.common.poi.ExcelView;
 import org.cotato.csquiz.domain.attendance.service.AttendanceExcelService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,18 +18,8 @@ public class AttendanceExcelController {
     private final AttendanceExcelService attendanceExcelService;
 
     @Operation(summary = "세션별 출석 기록 엑셀 다운로드 API")
-    @GetMapping
-    public ModelAndView downloadAttendanceRecordsAsExcelBySessions(
-            @RequestParam(name = "type") AttendanceExcelSheetType sheetType,
-            @RequestParam(name = "attendanceIds", required = false) List<Long> attendanceIds,
-            @RequestParam(name = "generationId", required = false) Long generationId) {
-
-        return switch (sheetType) {
-            case EACH -> new ModelAndView(new ExcelView(),
-                    attendanceExcelService.getAttendanceRecordsExcelDataBySessions(attendanceIds));
-            case GENERATION -> new ModelAndView(new ExcelView(),
-                    attendanceExcelService.getAttendanceRecordsExcelDataByGeneration(generationId));
-            default -> throw new AppException(ErrorCode.NO_SUCH_TYPE);
-        };
+    @GetMapping(params = "generationId")
+    public ModelAndView downloadAttendanceRecordsAsExcelBySessions(@RequestParam(name = "generationId") Long generationId) {
+        return new ModelAndView(new ExcelView(), attendanceExcelService.getAttendanceRecordsExcelDataByGeneration(generationId));
     }
 }
