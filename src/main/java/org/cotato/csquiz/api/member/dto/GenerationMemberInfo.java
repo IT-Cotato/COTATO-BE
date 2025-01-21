032638340bbe6d10533b9cd36d2b4809807c6b3d
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-01-21 20:59:15 +0900

    feat: 기수별 멤버 역할 필드 타입 변경 (#273)
    - MemberRole -> GenerationMemberRole

--- src/main/java/org/cotato/csquiz/api/member/dto/GenerationMemberInfo.java
+++ src/main/java/org/cotato/csquiz/api/member/dto/GenerationMemberInfo.java
@@ -2,15 +2,15 @@ package org.cotato.csquiz.api.member.dto;
 
 import org.cotato.csquiz.domain.auth.entity.Member;
 import org.cotato.csquiz.domain.auth.enums.MemberPosition;
-import org.cotato.csquiz.domain.auth.enums.MemberRole;
 import org.cotato.csquiz.domain.generation.entity.GenerationMember;
+import org.cotato.csquiz.domain.generation.enums.GenerationMemberRole;
 
 public record GenerationMemberInfo(
         Long generationMemberId,
         String name,
         MemberPosition position,
         Integer generationNumber,
-        MemberRole role
+        GenerationMemberRole role
 ) {
     public static GenerationMemberInfo from(GenerationMember generationMember) {
         Member member = generationMember.getMember();
