b1aec2d44a008d50ff134f6ac2c08b676f3d4592
youth <boysoeng@naver.com>
2025-01-23 18:47:22 +0900

    feat: 출결 기록 다운로드 API 매니저 권한 추가

--- src/main/java/org/cotato/csquiz/api/attendance/controller/AttendanceExcelController.java
+++ src/main/java/org/cotato/csquiz/api/attendance/controller/AttendanceExcelController.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.cotato.csquiz.common.poi.ExcelView;
+import org.cotato.csquiz.common.role.RoleAuthority;
 import org.cotato.csquiz.domain.attendance.service.AttendanceExcelService;
+import org.cotato.csquiz.domain.auth.enums.MemberRole;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,12 +20,14 @@ public class AttendanceExcelController {
 
     private final AttendanceExcelService attendanceExcelService;
 
+    @RoleAuthority(value = MemberRole.MANAGER)
     @Operation(summary = "세션별 출석 기록 엑셀 다운로드 API")
     @GetMapping(params = "generationId")
     public ModelAndView downloadAttendanceRecordsAsExcelByGeneration(@RequestParam(name = "generationId") Long generationId) {
         return new ModelAndView(new ExcelView(), attendanceExcelService.getAttendanceRecordsExcelDataByGeneration(generationId));
     }
 
+    @RoleAuthority(value = MemberRole.MANAGER)
     @Operation(summary = "출석별 출결 기록 엑셀 다운로드 API")
     @GetMapping(params = "attendanceIds")
     public ModelAndView downloadAttendanceRecordsAsExcelByAttendanceIds(@RequestParam(name = "attendanceIds") List<Long> attendanceIds) {
