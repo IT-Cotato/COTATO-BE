0c50bcb1ff821fc7396a69c87e1ee2083aba639c
youth <boysoeng@naver.com>
2025-01-27 01:05:47 +0900

    feat: 반환 필드 null 여부 추가

--- src/main/java/org/cotato/csquiz/api/generation/dto/GenerationInfoResponse.java
+++ src/main/java/org/cotato/csquiz/api/generation/dto/GenerationInfoResponse.java
@@ -1,13 +1,19 @@
 package org.cotato.csquiz.api.generation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import java.time.LocalDate;
 import org.cotato.csquiz.domain.generation.entity.Generation;
 
 public record GenerationInfoResponse(
+        @Schema(requiredMode = RequiredMode.REQUIRED)
         Long generationId,
+        @Schema(requiredMode = RequiredMode.REQUIRED)
         Integer generationNumber,
         Integer sessionCount,
+        @Schema(requiredMode = RequiredMode.REQUIRED)
         LocalDate startDate,
+        @Schema(requiredMode = RequiredMode.REQUIRED)
         LocalDate endDate
 ) {
 
