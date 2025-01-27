4377fa6f7135988d905e9679e5fb97f36eec8feb
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-01-28 00:55:35 +0900

    [COT-187] Feature: 프로필 정보 반환 API (#287)
    * feat: 마이페이지 정보 반환 시 프로필 사진 정보 제외
    
    * feat: 프로필 카드 정보 반환 API
    
    * refactor: 프로필 정보를 반환할 때 pathVariable으로 memberId 받아 반환하게 변경
    
    * feat: mypage 회원정보 반환 API에 중복된 정보 삭제

--- src/main/java/org/cotato/csquiz/domain/auth/repository/ProfileLinkRepository.java
+++ src/main/java/org/cotato/csquiz/domain/auth/repository/ProfileLinkRepository.java
@@ -1,9 +1,12 @@
 package org.cotato.csquiz.domain.auth.repository;
 
+import java.util.List;
 import org.cotato.csquiz.domain.auth.entity.Member;
 import org.cotato.csquiz.domain.auth.entity.ProfileLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileLinkRepository extends JpaRepository<ProfileLink, Long> {
     void deleteAllByMember(Member member);
+
+    List<ProfileLink> findAllByMember(Member member);
 }
