0fd2e55e4444b47b30c990895d43a628ab10f64b
youth <boysoeng@naver.com>
2025-02-06 16:51:33 +0900

    feat: 재활성화 되지 않은 탈퇴 요청 조회

---
+++ src/main/java/org/cotato/csquiz/domain/auth/service/component/MemberLeavingRequestReader.java
@@ -0,0 +1,22 @@
+package org.cotato.csquiz.domain.auth.service.component;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.cotato.csquiz.domain.auth.entity.Member;
+import org.cotato.csquiz.domain.auth.entity.MemberLeavingRequest;
+import org.cotato.csquiz.domain.auth.repository.MemberLeavingRequestRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberLeavingRequestReader {
+
+    private final MemberLeavingRequestRepository memberLeavingRequestRepository;
+
+    public MemberLeavingRequest getLeavingRequestByMember(final Member member) {
+        return memberLeavingRequestRepository.findByMemberAndIsReactivatedFalse(member)
+                .orElseThrow(() -> new EntityNotFoundException("해당 부원읭 요청이 존재하지 않습니다."));
+    }
+}
