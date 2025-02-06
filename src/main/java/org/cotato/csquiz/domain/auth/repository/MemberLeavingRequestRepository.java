06d535cfd8376806a4eff083a4b234a92296da05
Youth <109585620+Youthhing@users.noreply.github.com>
2025-02-06 20:32:30 +0900

    [COT-196] Feature: 비활성화 처리된 부원의 재활성화 요청 (#293)
    * feat: 재활성화 되지 않은 탈퇴 요청 조회
    
    * feat: 부원 재활성화 요청 API 구현
    
    * test: 부원 재활성화 요청 및 에러 처리
    
    * chore: 오타 수정

--- src/main/java/org/cotato/csquiz/domain/auth/repository/MemberLeavingRequestRepository.java
+++ src/main/java/org/cotato/csquiz/domain/auth/repository/MemberLeavingRequestRepository.java
@@ -1,7 +1,10 @@
 package org.cotato.csquiz.domain.auth.repository;
 
+import java.util.Optional;
+import org.cotato.csquiz.domain.auth.entity.Member;
 import org.cotato.csquiz.domain.auth.entity.MemberLeavingRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberLeavingRequestRepository extends JpaRepository<MemberLeavingRequest, Long> {
+    Optional<MemberLeavingRequest> findByMemberAndIsReactivatedFalse(Member member);
 }
