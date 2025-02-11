f08b0f240894a57cc75ed5b7d2755b4ea0eb467b
youth <boysoeng@naver.com>
2025-02-12 02:20:22 +0900

    feat: 최초 로그인 시 구독 요청 로직 구현

--- src/main/java/org/cotato/csquiz/common/sse/SseService.java
+++ src/main/java/org/cotato/csquiz/common/sse/SseService.java
@@ -1,10 +1,22 @@
 package org.cotato.csquiz.common.sse;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cotato.csquiz.domain.attendance.entity.Attendance;
+import org.cotato.csquiz.domain.attendance.enums.AttendanceOpenStatus;
 import org.cotato.csquiz.domain.attendance.repository.AttendanceRepository;
+import org.cotato.csquiz.domain.attendance.service.component.AttendanceReader;
+import org.cotato.csquiz.domain.attendance.util.AttendanceUtil;
+import org.cotato.csquiz.domain.auth.component.GenerationMemberAuthValidator;
+import org.cotato.csquiz.domain.auth.entity.Member;
+import org.cotato.csquiz.domain.generation.entity.Generation;
+import org.cotato.csquiz.domain.generation.entity.Session;
+import org.cotato.csquiz.domain.generation.enums.SessionType;
+import org.cotato.csquiz.domain.generation.service.component.GenerationReader;
+import org.cotato.csquiz.domain.generation.service.component.SessionReader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -15,17 +27,37 @@ public class SseService {
 
     private static final Long DEFAULT_TIMEOUT = 60 * 1000 * 60L;
 
+    private final GenerationReader generationReader;
+    private final GenerationMemberAuthValidator generationMemberAuthValidator;
     private final AttendanceRepository attendanceRepository;
     private final SseAttendanceRepository sseAttendanceRepository;
     private final SseSender sseSender;
+    private final SessionReader sessionReader;
+    private final AttendanceReader attendanceReader;
 
-    public SseEmitter subscribeAttendance(final Long memberId) {
+    public SseEmitter subscribeAttendance(final Member member) {
+        LocalDateTime now = LocalDateTime.now();
+        Generation currentGeneration = generationReader.findByDate(now.toLocalDate());
+        generationMemberAuthValidator.checkGenerationPermission(member, currentGeneration);
+
         SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
+        setBaseEmitterConfiguration(member.getId(), sseEmitter);
+        sseAttendanceRepository.save(member.getId(), sseEmitter);
 
-        setBaseEmitterConfiguration(memberId, sseEmitter);
-        sseAttendanceRepository.save(memberId, sseEmitter);
+        Optional<Session> maybeSession = sessionReader.getByDate(now.toLocalDate());
+        if (maybeSession.isEmpty()) {
+            sseSender.sendInitialAttendanceStatus(sseEmitter, null, AttendanceOpenStatus.CLOSED);
+            return sseEmitter;
+        }
 
-        sseSender.sendInitialAttendanceStatus(sseEmitter);
+        Session session = maybeSession.get();
+        if (session.getSessionType() == SessionType.NO_ATTEND) {
+            sseSender.sendInitialAttendanceStatus(sseEmitter, null, AttendanceOpenStatus.CLOSED);
+            return sseEmitter;
+        }
+
+        Attendance attendance = attendanceReader.findBySession(session);
+        sseSender.sendInitialAttendanceStatus(sseEmitter, attendance.getId(), AttendanceUtil.getAttendanceOpenStatus(session.getSessionDateTime(), attendance, now));
 
         return sseEmitter;
     }
