4d0d29b4e0e007e8047d197f33ab50e9796d33c8
gikhoon <ghikhoon@naver.com>
2025-02-13 16:11:54 +0900

    feat: 세션 추가시 출석 정보 nullable 허용
    - null일 시 default 시간으로 설정

--- src/main/java/org/cotato/csquiz/api/session/dto/AddSessionRequest.java
+++ src/main/java/org/cotato/csquiz/api/session/dto/AddSessionRequest.java
@@ -1,11 +1,15 @@
 package org.cotato.csquiz.api.session.dto;
 
+import static org.cotato.csquiz.domain.attendance.enums.DeadLine.DEFAULT_ATTENDANCE_DEADLINE;
+import static org.cotato.csquiz.domain.attendance.enums.DeadLine.DEFAULT_LATE_DEADLINE;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import org.cotato.csquiz.domain.generation.enums.CSEducation;
 import org.cotato.csquiz.domain.generation.enums.DevTalk;
 import org.cotato.csquiz.domain.generation.enums.ItIssue;
@@ -35,12 +39,10 @@ public record AddSessionRequest(
 
         @Schema(example = "2024-11-11T19:10:00", description = "출석 마감 시간, 해당 시간 이후 지각  처리")
         @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        @NotNull
         LocalDateTime attendanceDeadLine,
 
         @Schema(example = "2024-11-11T19:20:00", description = "지각 마감 시간, 해당 시간 이후 결석 처리")
         @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        @NotNull
         LocalDateTime lateDeadLine,
 
         ItIssue itIssue,
@@ -48,4 +50,12 @@ public record AddSessionRequest(
         CSEducation csEducation,
         DevTalk devTalk
 ) {
+    public AddSessionRequest {
+        if (Objects.isNull(attendanceDeadLine)) {
+            attendanceDeadLine = LocalDateTime.of(sessionDateTime.toLocalDate(), DEFAULT_ATTENDANCE_DEADLINE.getTime());
+        }
+        if (Objects.isNull(lateDeadLine)) {
+            lateDeadLine = LocalDateTime.of(sessionDateTime.toLocalDate(), DEFAULT_LATE_DEADLINE.getTime());
+        }
+    }
 }
