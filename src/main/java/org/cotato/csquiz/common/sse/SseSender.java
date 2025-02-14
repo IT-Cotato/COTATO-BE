d547cc15dbed2e08d68c803d78881b45a8aacabf
youth <boysoeng@naver.com>
2025-02-14 20:31:04 +0900

    feat: sse emitter에 통한 출결 알림 전송

--- src/main/java/org/cotato/csquiz/common/sse/SseSender.java
+++ src/main/java/org/cotato/csquiz/common/sse/SseSender.java
@@ -1,27 +1,32 @@
 package org.cotato.csquiz.common.sse;
 
-import jakarta.persistence.EntityNotFoundException;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.cotato.csquiz.api.event.dto.AttendanceStatusInfo;
 import org.cotato.csquiz.common.error.ErrorCode;
 import org.cotato.csquiz.common.error.exception.AppException;
 import org.cotato.csquiz.domain.attendance.entity.Attendance;
 import org.cotato.csquiz.domain.attendance.enums.AttendanceOpenStatus;
-import org.cotato.csquiz.domain.attendance.repository.AttendanceRepository;
+import org.cotato.csquiz.domain.generation.entity.AttendanceNotification;
+import org.cotato.csquiz.domain.generation.entity.Session;
+import org.cotato.csquiz.domain.generation.repository.AttendanceNotificationRepository;
+import org.cotato.csquiz.domain.generation.service.component.SessionReader;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter.DataWithMediaType;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SseSender {
 
     private static final String ATTENDANCE_STATUS = "AttendanceStatus";
     private final SseAttendanceRepository sseAttendanceRepository;
-    private final AttendanceRepository attendanceRepository;
+    private final SessionReader sessionReader;
+    private final AttendanceNotificationRepository sessionNotificationRepository;
 
     public void sendInitialAttendanceStatus(final SseEmitter sseEmitter, final Long attendanceId, final AttendanceOpenStatus openStatus) {
         Set<DataWithMediaType> event = SseEmitter.event()
@@ -34,10 +39,10 @@ public class SseSender {
         send(sseEmitter, event);
     }
 
-    // sessionDateTime 7시에 출결을 구독 중인 부원들에게 출결 입력 시작 알림을 전송한다.
-    public void sendNotification(LocalDateTime notificationDate) {
-        Attendance attendance = attendanceRepository.findByAttendanceDeadLineDate(notificationDate)
-                .orElseThrow(() -> new EntityNotFoundException("해당 날짜에 진행하는 출석이 없습니다."));
+    @Transactional
+    public void sendAttendanceStartNotification(final AttendanceNotification attendanceNotification) {
+        Attendance attendance = attendanceNotification.getAttendance();
+        Session session = sessionReader.findById(attendance.getSessionId());
 
         Set<DataWithMediaType> data = SseEmitter.event()
                 .name(ATTENDANCE_STATUS)
@@ -51,6 +56,9 @@ public class SseSender {
         for (SseEmitter sseEmitter : sseEmitters) {
             send(sseEmitter, data);
         }
+        log.info("[send attendance notification: session id <{}>, time <{}>]", attendance.getSessionId(), session.getSessionDateTime());
+        attendanceNotification.done();
+        sessionNotificationRepository.save(attendanceNotification);
     }
 
     private void send(SseEmitter sseEmitter, Set<DataWithMediaType> data) {
