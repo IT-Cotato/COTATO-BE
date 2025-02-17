f300277304f57749844383b8e8f055ac83de7d17
Youth <109585620+Youthhing@users.noreply.github.com>
2025-02-17 11:42:58 +0900

    [COT-218] Feature: 삭제된 API 및 DTO 필드 제거 (#308)

--- src/main/java/org/cotato/csquiz/api/admin/dto/MemberApproveRequest.java
+++ src/main/java/org/cotato/csquiz/api/admin/dto/MemberApproveRequest.java
@@ -4,9 +4,6 @@ import org.cotato.csquiz.domain.auth.enums.MemberPosition;
 import jakarta.validation.constraints.NotNull;
 
 public record MemberApproveRequest(
-        @Deprecated(since = "신입 감자 가입 승인 개발 이후")
-        @NotNull
-        Long memberId,
         @NotNull
         MemberPosition position,
         @NotNull
