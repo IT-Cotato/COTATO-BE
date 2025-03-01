47e582231764e9f3943a2af01cd7931fe9fe7ce4
youth <boysoeng@naver.com>
2025-03-01 17:20:26 +0900

    test: 회원 상태에 따른 목록 조회 API 페이징 적용

--- src/test/java/org/cotato/csquiz/domain/auth/service/MemberServiceTest.java
+++ src/test/java/org/cotato/csquiz/domain/auth/service/MemberServiceTest.java
@@ -1,12 +1,16 @@
 package org.cotato.csquiz.domain.auth.service;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import org.cotato.csquiz.common.error.exception.AppException;
 import org.cotato.csquiz.domain.auth.entity.Member;
 import org.cotato.csquiz.domain.auth.entity.MemberLeavingRequest;
 import org.cotato.csquiz.domain.auth.enums.MemberStatus;
+import org.cotato.csquiz.domain.auth.repository.MemberRepository;
 import org.cotato.csquiz.domain.auth.service.component.MemberLeavingRequestReader;
 import org.cotato.csquiz.domain.auth.service.component.MemberReader;
 import org.junit.jupiter.api.Assertions;
@@ -14,6 +18,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.parameters.P;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -23,10 +31,16 @@ class MemberServiceTest {
     private MemberService memberService;
 
     @Mock
+    private EncryptService encryptService;
+
+    @Mock
     private MemberLeavingRequestReader memberLeavingRequestReader;
 
     @Mock
     private MemberReader memberReader;
+
+    @Mock
+    private MemberRepository memberRepository;
 
     @Test
     void 부원_활성화_요청() {
@@ -62,5 +76,25 @@ class MemberServiceTest {
 
         // when, then
         Assertions.assertThrows(AppException.class, () -> memberService.activateMember(member.getId()));
+    }
+
+    @Test
+    void 상태에_따른_부원_목록_조회() {
+        // given
+        Member member1 = Member.defaultMember("email1", "password1", "name1", "1");
+        Member member2 = Member.defaultMember("email2", "password2", "name2", "2");
+        member1.updateStatus(MemberStatus.APPROVED);
+        member2.updateStatus(MemberStatus.APPROVED);
+
+        PageImpl<Member> members = new PageImpl<>(List.of(member1, member2));
+        when(memberRepository.findAllByStatus(MemberStatus.APPROVED, Pageable.ofSize(2))).thenReturn(members);
+        when(encryptService.decryptPhoneNumber(any())).thenReturn("01012345678");
+
+        // when
+        var approvedMembers = memberService.getMembersByStatus(MemberStatus.APPROVED, Pageable.ofSize(2));
+
+        // then
+        Assertions.assertEquals(2, approvedMembers.getNumberOfElements());
+        verify(memberRepository).findAllByStatus(MemberStatus.APPROVED, Pageable.ofSize(2));
     }
 }
