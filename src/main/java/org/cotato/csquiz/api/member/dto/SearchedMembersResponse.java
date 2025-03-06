1efa3fd608a7d3ac274eb2c3ec11226ce186233a
youth <boysoeng@naver.com>
2025-03-07 00:06:40 +0900

    feat: OM 목록 검색 API 구현

--- src/main/java/org/cotato/csquiz/api/member/dto/SearchedMembersResponse.java
+++ src/main/java/org/cotato/csquiz/api/member/dto/SearchedMembersResponse.java
@@ -3,11 +3,11 @@ package org.cotato.csquiz.api.member.dto;
 import java.util.List;
 import org.cotato.csquiz.domain.auth.entity.Member;
 
-public record AddableMembersResponse(
+public record SearchedMembersResponse(
         List<SearchedMemberInfo> memberInfos
 ) {
-    public static AddableMembersResponse from(List<Member> members) {
-        return new AddableMembersResponse(members.stream()
+    public static SearchedMembersResponse from(List<Member> members) {
+        return new SearchedMembersResponse(members.stream()
                 .map(SearchedMemberInfo::from)
                 .toList());
     }
