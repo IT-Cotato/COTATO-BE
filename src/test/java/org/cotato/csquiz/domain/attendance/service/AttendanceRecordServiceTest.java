6380e4ce041bb925522221754af0b3b47ae48312
youth <boysoeng@naver.com>
2025-04-02 21:44:55 +0900

    [COT-253] Hotfix: 출결 기록 변경 시 세션 타입과 변경할 결과 비교 로직 에러 해결 (#334)
    * fix: 출결 기록 변경이 되지 않는 문제 해결
    
    * chore: 메서드 명 변경
    
    * test: 출결 기록 변경
    
    * chore: remove unused method
    
    * feat: 출결 기록 변경 시 타입 체크 로직 추가
    
    * test: 타입에 맞지 않는 출결 결과 변경 시 예외 발생

---
+++ src/test/java/org/cotato/csquiz/domain/attendance/service/AttendanceRecordServiceTest.java
@@ -0,0 +1,86 @@
+package org.cotato.csquiz.domain.attendance.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.cotato.csquiz.common.error.ErrorCode;
+import org.cotato.csquiz.common.error.exception.AppException;
+import org.cotato.csquiz.domain.attendance.entity.Attendance;
+import org.cotato.csquiz.domain.attendance.entity.AttendanceRecord;
+import org.cotato.csquiz.domain.attendance.enums.AttendanceResult;
+import org.cotato.csquiz.domain.attendance.repository.AttendanceRecordRepository;
+import org.cotato.csquiz.domain.attendance.service.component.AttendanceReader;
+import org.cotato.csquiz.domain.attendance.service.component.AttendanceRecordReader;
+import org.cotato.csquiz.domain.auth.entity.Member;
+import org.cotato.csquiz.domain.auth.service.component.MemberReader;
+import org.cotato.csquiz.domain.generation.entity.Session;
+import org.cotato.csquiz.domain.generation.enums.SessionType;
+import org.cotato.csquiz.domain.generation.service.component.SessionReader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class AttendanceRecordServiceTest {
+
+    @InjectMocks
+    private AttendanceRecordService attendanceRecordService;
+
+    @Mock
+    private AttendanceReader attendanceReader;
+
+    @Mock
+    private AttendanceRecordReader attendanceRecordReader;
+
+    @Mock
+    private SessionReader sessionReader;
+
+    @Mock
+    private MemberReader memberReader;
+
+    @Mock
+    private AttendanceRecordRepository attendanceRecordRepository;
+
+    @Test
+    void 세션_타입에_맞는_출결_기록_변경() {
+        // given
+        Session session = Session.builder().sessionType(SessionType.OFFLINE).build();
+        Attendance attendance = Attendance.builder().session(session).build();
+        AttendanceRecord attendanceRecord = AttendanceRecord.absentRecord(attendance, 1L);
+        Member member = Member.defaultMember("test", "test", "test", "test");
+
+        when(attendanceReader.findById(any())).thenReturn(attendance);
+        when(attendanceRecordReader.getByAttendanceAndMember(any(), any())).thenReturn(Optional.of(attendanceRecord));
+        when(sessionReader.getByAttendance(attendance)).thenReturn(session);
+        when(memberReader.findById(any())).thenReturn(member);
+
+        // when
+        attendanceRecordService.updateAttendanceRecord(1L, 1L, AttendanceResult.OFFLINE);
+
+        // then
+        Assertions.assertEquals(AttendanceResult.OFFLINE, attendanceRecord.getAttendanceResult());
+    }
+
+    @Test
+    void 세션_타입에_맞지_않는_출결_기록_변경() {
+        // given
+        Session session = Session.builder().sessionType(SessionType.ONLINE).build();
+        Attendance attendance = Attendance.builder().session(session).build();
+        AttendanceRecord attendanceRecord = AttendanceRecord.absentRecord(attendance, 1L);
+        Member member = Member.defaultMember("test", "test", "test", "test");
+
+        when(attendanceReader.findById(any())).thenReturn(attendance);
+        when(attendanceRecordReader.getByAttendanceAndMember(any(), any())).thenReturn(Optional.of(attendanceRecord));
+        when(sessionReader.getByAttendance(attendance)).thenReturn(session);
+        when(memberReader.findById(any())).thenReturn(member);
+
+        // when, then
+        AppException appException = Assertions.assertThrows(AppException.class,
+                () -> attendanceRecordService.updateAttendanceRecord(1L, 1L, AttendanceResult.OFFLINE));
+        Assertions.assertEquals(ErrorCode.INVALID_RECORD_UPDATE, appException.getErrorCode());
+    }
+}
