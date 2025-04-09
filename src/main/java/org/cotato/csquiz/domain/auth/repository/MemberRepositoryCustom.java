d06ae1847163bf0bbd8aafee2f740564c1949940
Youth <109585620+Youthhing@users.noreply.github.com>
2025-04-10 00:48:06 +0900

    [COT-259] Feature: OM 검색 API 페이징 적용 (#339)
    * feat: 페이징을 적용한 부원 목록 검색 로직 구현
    
    * feat: 페이징을 적용한 부원 목록 검색 API 구현
    
    * feat: 부원 목록 검색 API 권한 추가
    
    * test: OM 부원 목록 조회 API

--- src/main/java/org/cotato/csquiz/domain/auth/repository/MemberRepositoryCustom.java
+++ src/main/java/org/cotato/csquiz/domain/auth/repository/MemberRepositoryCustom.java
@@ -3,7 +3,12 @@ package org.cotato.csquiz.domain.auth.repository;
 import java.util.List;
 import org.cotato.csquiz.domain.auth.entity.Member;
 import org.cotato.csquiz.domain.auth.enums.MemberPosition;
+import org.cotato.csquiz.domain.auth.enums.MemberStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface MemberRepositoryCustom {
     List<Member> findAllWithFilters(Integer passedGenerationNumber, MemberPosition memberPosition, String name);
+
+    Page<Member> findAllWithFiltersPageable(Integer passedGenerationNumber, MemberPosition memberPosition, MemberStatus memberStatus, String name, Pageable pageable);
 }
