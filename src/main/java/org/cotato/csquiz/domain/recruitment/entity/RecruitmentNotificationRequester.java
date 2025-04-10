0fef0dd492ca1379b0ee9127e43a891e3b07990e
gikhoon <ghikhoon@naver.com>
2025-04-10 15:40:58 +0900

    refactor: 모집 알림 신청자 엔티티, 컬럼명 수정

--- src/main/java/org/cotato/csquiz/domain/recruitment/entity/RecruitmentNotificationRequester.java
+++ src/main/java/org/cotato/csquiz/domain/recruitment/entity/RecruitmentNotificationRequester.java
@@ -15,21 +15,21 @@ import org.cotato.csquiz.domain.recruitment.enums.SendStatus;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class EmailApplicant extends BaseTimeEntity {
+public class RecruitmentNotificationRequester extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "applicant_id")
+    @Column(name = "requester_id")
     private Long id;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "request_email", nullable = false)
     private String email;
 
-    @Column(name = "apply_time", nullable = false)
-    private LocalDateTime applyTime;
+    @Column(name = "request_time", nullable = false)
+    private LocalDateTime requestTime;
 
     @Column(name = "policy_checked", nullable = false)
-    private boolean policyChecked;
+    private Boolean policyChecked;
 
     @Column(name = "send_status", nullable = false)
     private SendStatus sendStatus;
