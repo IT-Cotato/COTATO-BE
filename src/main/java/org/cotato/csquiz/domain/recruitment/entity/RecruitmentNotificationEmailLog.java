84be7b5534682ac5524710f2e4f8f1cbe122e780
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-04-13 23:44:27 +0900

    [COT-260] Feature: 모입 이메일 전송을 위한 엔티티 구축 (#338)
    * refactor: 일반화를 위해 클래스 명 변경
    
    GenerationPeriod.java -> Period.java
    
    * feat: Join us 관련 DB 구축
    
    * Revert "refactor: 일반화를 위해 클래스 명 변경"
    
    This reverts commit e279fb39eb56e503f1eee4f170f77cf1baa04af4.
    
    * refactor: 일반화를 위해 클래스 명 변경
    
    * refactor: 모집 정보 엔티티, 컬럼 명 변경
    
    * refactor: 모집 알림 메일 전송 요청 엔티티 명 수정
    
    * refactor: 모집 알림 신청자 엔티티, 컬럼명 수정
    
    * refactor: 메일 전송 로그 엔티티 수정
    
    * refactor: 모집 정보 컬럼 네임 삭제
    
    * refactor: 모집 이메일 로그 컬럼명 오타 수정
    
    receive_id -> receiver_id
    
    (cherry picked from commit 71385c2749437f4ffba29b8d78901bbc076a0810)

---
+++ src/main/java/org/cotato/csquiz/domain/recruitment/entity/RecruitmentNotificationEmailLog.java
@@ -0,0 +1,36 @@
+package org.cotato.csquiz.domain.recruitment.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.cotato.csquiz.common.entity.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecruitmentNotificationEmailLog extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "log_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private RecruitmentNotificationRequester receiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "notification_id", nullable = false)
+    private RecruitmentNotification notification;
+
+    @Column(name = "send_success")
+    private Boolean sendSuccess;
+}
---
+++ src/main/java/org/cotato/csquiz/domain/recruitment/enums/SendStatus.java
@@ -0,0 +1,15 @@
+package org.cotato.csquiz.domain.recruitment.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SendStatus {
+    NOT_SENT("안보냄"),
+    FAIL("전송 실패"),
+    SUCCESS("전송 성공")
+    ;
+
+    private final String description;
+}
