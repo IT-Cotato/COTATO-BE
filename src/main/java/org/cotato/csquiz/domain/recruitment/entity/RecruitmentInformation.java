945740da62dd42cc041a122ed5c8151c70c62bb9
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-04-13 23:44:27 +0900

    [COT-260] Feature: 모입 이메일 전송을 위한 엔티티 구축 (#338)
    * refactor: 일반화를 위해 클래스 명 변경
    
    GenerationPeriod.java -> Period.java
    
    * feat: Join us 관련 DB 구축
    
    * Revert "refactor: 일반화를 위해 클래스 명 변경"
    
    This reverts commit e279fb39eb56e503f1eee4f170f77cf1baa04af4.
    
    * refactor: 일반화를 위해 클래스 명 변경
    
    * refactor: 모집 정보 엔티티, 컬럼 명 변경
    
    * refactor: 모집 알림 메일 전송 요청 엔티티 명 수정
    
    * refactor: 모집 알림 신청자 엔티티, 컬럼명 수정
    
    * refactor: 메일 전송 로그 엔티티 수정
    
    * refactor: 모집 정보 컬럼 네임 삭제
    
    * refactor: 모집 이메일 로그 컬럼명 오타 수정
    
    receive_id -> receiver_id
    
    (cherry picked from commit 71385c2749437f4ffba29b8d78901bbc076a0810)

---
+++ src/main/java/org/cotato/csquiz/domain/recruitment/entity/RecruitmentInformation.java
@@ -0,0 +1,38 @@
+package org.cotato.csquiz.domain.recruitment.entity;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.cotato.csquiz.common.entity.BaseTimeEntity;
+import org.cotato.csquiz.domain.generation.embedded.Period;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecruitmentInformation extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "is_opened")
+    private Boolean isOpened;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "startDate", column = @Column(name = "recruitment_start_date")),
+            @AttributeOverride(name = "endDate", column = @Column(name = "recruitment_end_date"))
+    })
+    private Period period;
+
+    @Column(name = "recruitment_url", columnDefinition = "TEXT")
+    private String recruitmentUrl;
+}
