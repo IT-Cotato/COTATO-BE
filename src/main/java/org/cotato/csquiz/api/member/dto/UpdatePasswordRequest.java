ed591a47e1937f24d66533de467e5f06798189f2
youth <boysoeng@naver.com>
2025-04-15 16:22:10 +0900

    feat: 비밀번호 변경 로직 구분

--- src/main/java/org/cotato/csquiz/api/member/dto/UpdatePasswordRequest.java
+++ src/main/java/org/cotato/csquiz/api/member/dto/UpdatePasswordRequest.java
@@ -1,9 +1,11 @@
 package org.cotato.csquiz.api.member.dto;
 
 import jakarta.validation.constraints.NotNull;
+import org.cotato.csquiz.common.validator.Password;
 
 public record UpdatePasswordRequest(
         @NotNull
+        @Password
         String password
 ) {
 }
