0cdc920ab71151deb0953e1c139edd5e68cca2ba
gikhoon <gikhoon@gmail.com>
2025-04-18 14:31:40 +0900

    feat: 모집 정보가 1개가 아니라면 예외 발생하게 변경

--- src/main/java/org/cotato/csquiz/domain/recruitment/service/component/RecruitmentInformationReader.java
+++ src/main/java/org/cotato/csquiz/domain/recruitment/service/component/RecruitmentInformationReader.java
@@ -1,6 +1,9 @@
 package org.cotato.csquiz.domain.recruitment.service.component;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.cotato.csquiz.common.error.ErrorCode;
+import org.cotato.csquiz.common.error.exception.AppException;
 import org.cotato.csquiz.domain.recruitment.entity.RecruitmentInformation;
 import org.cotato.csquiz.domain.recruitment.repository.RecruitmentInformationRepository;
 import org.springframework.stereotype.Component;
@@ -14,7 +17,10 @@ public class RecruitmentInformationReader {
     private final RecruitmentInformationRepository recruitmentInformationRepository;
 
     public RecruitmentInformation findRecruitmentInformation() {
-        return recruitmentInformationRepository.findAll()
-                .get(0);
+        List<RecruitmentInformation> infos = recruitmentInformationRepository.findAll();
+        if (infos.size() != 1) {
+            throw new AppException(ErrorCode.RECRUITMENT_INFO_COUNT_INVALID);
+        }
+        return infos.get(0);
     }
 }
