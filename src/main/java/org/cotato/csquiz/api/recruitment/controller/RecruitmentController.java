b09bb52a3c1f6a24d87a28a1447b91cbb8bd5b54
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-04-20 14:42:24 +0900

    [COT-268] Feature: 모집 정보 반환 API 구현 (#342)
    * feat: 모집 정보 오픈 여부 nullable false 설정
    
    * feat: 모집 정보 반환 API 구현
    
    * feat: 모집 정보 엔티티 빌더 추가
    
    * test: 모집 정보 반환 API 테스트 코드 작성
    
    * feat: 스프링 실행 시 모집 정보가 없으면 초기값 추가
    
    * feat: 사용하지 않은 import문 삭제
    
    * refactor: 모집 관련 API path 변경
    
    * feat: 모집 정보가 1개가 아니라면 예외 발생하게 변경
    
    * Revert "feat: 스프링 실행 시 모집 정보가 없으면 초기값 추가"
    
    This reverts commit 24e5577c16b55d339fbfce9c7d683e6e3df5db50.
    
    * refactor: 모집 정보 서비스의 클래스 단위 트랜잭션 선언 삭제
    
    (cherry picked from commit 1c3e47d026fe00382468b00197abd2db4202257c)
    (cherry picked from commit 628038067bd0fe1b2e1940b165d40158653d08ef)

---
+++ src/main/java/org/cotato/csquiz/api/recruitment/controller/RecruitmentController.java
@@ -0,0 +1,28 @@
+package org.cotato.csquiz.api.recruitment.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.cotato.csquiz.api.recruitment.dto.RecruitmentInfoResponse;
+import org.cotato.csquiz.domain.recruitment.service.RecruitmentInformationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@Tag(name = "모집 정보", description = "모집 관련 API 입니다.")
+@RequestMapping("/v2/api/recruitments")
+@RequiredArgsConstructor
+public class RecruitmentController {
+
+    private final RecruitmentInformationService recruitmentInformationService;
+
+    @GetMapping
+    @Operation(summary = "모집 정보 반환 API")
+    public ResponseEntity<RecruitmentInfoResponse> findRecruitmentInfo() {
+        return ResponseEntity.ok().body(recruitmentInformationService.findRecruitmentInfo());
+    }
+}
