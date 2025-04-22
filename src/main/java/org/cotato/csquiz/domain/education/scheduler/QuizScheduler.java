646ebcd95c7bb8ac5170954d6a56c5887ce64e44
youth <boysoeng@naver.com>
2025-04-22 17:04:48 +0900

    feat: 랜덤 퀴즈 전송 스케줄러 수정

--- src/main/java/org/cotato/csquiz/domain/education/scheduler/QuizScheduler.java
+++ src/main/java/org/cotato/csquiz/domain/education/scheduler/QuizScheduler.java
@@ -2,9 +2,10 @@ package org.cotato.csquiz.domain.education.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.cotato.csquiz.domain.education.entity.Quiz;
 import org.cotato.csquiz.domain.education.service.ChoiceService;
 import org.cotato.csquiz.domain.education.service.DiscordService;
-import org.cotato.csquiz.domain.education.service.QuizService;
+import org.cotato.csquiz.domain.education.service.component.QuizReader;
 import org.cotato.csquiz.domain.education.service.dto.RandomQuizResponse;
 import org.cotato.csquiz.domain.education.util.DiscordUtil;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,13 +18,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class QuizScheduler {
 
     private final DiscordService discordService;
-    private final QuizService quizService;
+    private final QuizReader quizReader;
     private final ChoiceService choiceService;
 
     @Transactional(readOnly = true)
     @Scheduled(cron = "0 0 14 * * ?")
     public void sendRandomQuiz() {
-        RandomQuizResponse randomQuiz = quizService.pickRandomQuiz();
+        Quiz quiz = quizReader.getRandomDiscordQuiz();
+        RandomQuizResponse randomQuiz = RandomQuizResponse.from(quiz);
+
         discordService.sendMessageToTextChannel(DiscordUtil.getMultipleQuizEmbeds(randomQuiz),
                 DiscordUtil.getButtons(choiceService.findAllChoices(randomQuiz.id())));
         log.info("[디스코드 채널 문제 전송 완료: {}]", randomQuiz.id());
