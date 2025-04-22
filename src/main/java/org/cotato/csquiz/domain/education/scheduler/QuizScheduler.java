6bd7291983e647aa060fa026a426d4446142a859
youth <boysoeng@naver.com>
2025-04-22 17:35:39 +0900

    chore: remove unused import line

--- src/main/java/org/cotato/csquiz/domain/education/scheduler/QuizScheduler.java
+++ src/main/java/org/cotato/csquiz/domain/education/scheduler/QuizScheduler.java
@@ -1,6 +1,5 @@
 package org.cotato.csquiz.domain.education.scheduler;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cotato.csquiz.domain.education.entity.Quiz;
