9451f0bc17823444bb7841d775627d3c71371125
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-04-24 13:36:58 +0900

    [COT-263] Feature: 모집 알림 신청 API 구현 (#343)
    * feat: 모집 알림 엔티티 Enum 타입 수정
    
    to EnumType.STRING
    
    * feat: 모집 알림 신청 API 구현
    
    * test: 모집 알림 신청 테스트 코드 작성
    
    * feat: 클래스 단위 트랜잭션 삭제

--- src/main/java/org/cotato/csquiz/api/recruitment/controller/RecruitmentController.java
+++ src/main/java/org/cotato/csquiz/api/recruitment/controller/RecruitmentController.java
@@ -2,12 +2,17 @@ package org.cotato.csquiz.api.recruitment.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cotato.csquiz.api.recruitment.dto.RecruitmentInfoResponse;
+import org.cotato.csquiz.api.recruitment.dto.RequestRecruitmentNotificationRequest;
 import org.cotato.csquiz.domain.recruitment.service.RecruitmentInformationService;
+import org.cotato.csquiz.domain.recruitment.service.RecruitmentNotificationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +24,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecruitmentController {
 
     private final RecruitmentInformationService recruitmentInformationService;
+    private final RecruitmentNotificationService recruitmentNotificationService;
 
     @GetMapping
     @Operation(summary = "모집 정보 반환 API")
     public ResponseEntity<RecruitmentInfoResponse> findRecruitmentInfo() {
         return ResponseEntity.ok().body(recruitmentInformationService.findRecruitmentInfo());
+    }
+
+    @PostMapping("/notification")
+    @Operation(summary = "모집 알림 신청 API")
+    public ResponseEntity<Void> requestRecruitmentNotification(
+            @RequestBody @Valid RequestRecruitmentNotificationRequest request) {
+        recruitmentNotificationService.requestRecruitmentNotification(request.email(), request.policyCheck());
+        return ResponseEntity.noContent().build();
     }
 }
