8f7728fb94bf78052b03a62978bd1a19a5875765
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-04-24 13:50:51 +0900

    [COT-267] Feature: 모집 상태 수정 API 구현 (#345)
    * feat: 모집 정보 수정 API 구현
    
    * feat: request 에 맞게 putmapping으로 데이터 수정하게 변경
    
    * feat: 시작 날짜가 끝 날짜보다 뒤면 예외 발생
    
    * fix: merge로 누락된 에러코드 작성
    
    (cherry picked from commit 29b4ccc839e6d443d5807988cfdfc404fbf51c83)
    (cherry picked from commit 37f7acc882bc7fbf8be98e9b73c5b0154bd8ece6)

--- src/main/java/org/cotato/csquiz/domain/recruitment/service/RecruitmentInformationService.java
+++ src/main/java/org/cotato/csquiz/domain/recruitment/service/RecruitmentInformationService.java
@@ -1,7 +1,11 @@
 package org.cotato.csquiz.domain.recruitment.service;
 
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.cotato.csquiz.api.recruitment.dto.RecruitmentInfoResponse;
+import org.cotato.csquiz.common.error.ErrorCode;
+import org.cotato.csquiz.common.error.exception.AppException;
+import org.cotato.csquiz.domain.generation.embedded.Period;
 import org.cotato.csquiz.domain.recruitment.entity.RecruitmentInformation;
 import org.cotato.csquiz.domain.recruitment.service.component.RecruitmentInformationReader;
 import org.springframework.stereotype.Service;
@@ -20,5 +24,28 @@ public class RecruitmentInformationService {
             return RecruitmentInfoResponse.opened(info);
         }
         return RecruitmentInfoResponse.closed();
+    }
+
+    @Transactional
+    public void changeRecruitmentInfo(final Boolean isOpened, final LocalDate startDate, final LocalDate endDate,
+                                      String recruitmentUrl) {
+        RecruitmentInformation info = recruitmentInformationReader.findRecruitmentInformation();
+
+        if (isOpened) {
+            validateOpenParameters(startDate, endDate, recruitmentUrl);
+        }
+
+        info.changeOpened(isOpened);
+        info.changePeriod(Period.of(startDate, endDate));
+        info.changeRecruitmentUrl(recruitmentUrl);
+    }
+
+    private void validateOpenParameters(LocalDate startDate, LocalDate endDate, String recruitmentUrl) {
+        if (startDate == null || endDate == null || recruitmentUrl == null || recruitmentUrl.isBlank()) {
+            throw new AppException(ErrorCode.INVALID_RECRUITMENT_INFO);
+        }
+        if (startDate.isAfter(endDate)) {
+            throw new AppException(ErrorCode.INVALID_DATE);
+        }
     }
 }
