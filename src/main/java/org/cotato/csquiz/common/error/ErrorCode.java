ebae638454d41c599faff12e0e37869fb31deba6
gikhoon <gikhoon@gmail.com>
2025-04-24 13:47:49 +0900

    fix: merge로 누락된 에러코드 작성

--- src/main/java/org/cotato/csquiz/common/error/ErrorCode.java
+++ src/main/java/org/cotato/csquiz/common/error/ErrorCode.java
@@ -113,6 +113,7 @@ public enum ErrorCode {
     //모집 알림 관련
     INVALID_RECRUITMENT_INFO(HttpStatus.BAD_REQUEST, "RE-101", "모집 수정 정보를 작성하세요."),
     ALREADY_REQUEST_NOTIFICATION(HttpStatus.CONFLICT, "RE-301", "이미 모집 신청을 완료했습니다"),
+    RECRUITMENT_INFO_COUNT_INVALID(HttpStatus.CONFLICT, "RE-302", "모집 정보 갯수가 없거나 2개 이상힙니디."),
 
     // 500 오류 -> 서버측에서 처리가 실패한 부분들
     WEBSOCKET_SEND_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "S-001", "소캣 메세지 전송 실패"),
