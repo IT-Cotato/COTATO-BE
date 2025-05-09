d98a1d98a9b45377bab8493e427684754332d58d
gikhoon <gikhoon@gmail.com>
2025-05-09 13:35:21 +0900

    refactor: run-async, exceptionally로 메일 전송 로직 수정

--- src/main/java/org/cotato/csquiz/domain/recruitment/service/component/RecruitmentNotificationSender.java
+++ src/main/java/org/cotato/csquiz/domain/recruitment/service/component/RecruitmentNotificationSender.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.cotato.csquiz.common.email.SesEmailSender;
 import org.cotato.csquiz.domain.recruitment.email.EmailContent;
 import org.cotato.csquiz.domain.recruitment.entity.RecruitmentNotificationRequester;
-import org.cotato.csquiz.domain.recruitment.enums.SendStatus;
 import org.cotato.csquiz.domain.recruitment.service.component.dto.NotificationResult;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -21,19 +20,21 @@ public class RecruitmentNotificationSender {
     @Async("emailSendThreadPoolExecutor")
     public CompletableFuture<NotificationResult> sendNotificationAsync(final RecruitmentNotificationRequester requester,
                                                                        final EmailContent emailContent) {
-        boolean success = true;
-        try {
-            sesEmailSender.sendRawMessageBody(
-                    requester.getEmail(),
-                    emailContent.htmlBody(),
-                    emailContent.subject()
-            );
-        } catch (Exception e) {
-            success = false;
-            requester.updateSendStatus(SendStatus.FAIL);
-            log.info("메일 전송 실패 email: {} exception: {}", requester.getEmail(), e.getMessage());
-        }
+        return sendEmailAsync(requester, emailContent)
+                .thenApply(v -> NotificationResult.of(requester.getId(), true))
+                .exceptionally(e -> {
+                    log.info("메일 전송 실패 email: {} exception: {}", requester.getEmail(), e.getMessage());
+                    return NotificationResult.of(requester.getId(), false);
+                });
+    }
 
-        return CompletableFuture.completedFuture(NotificationResult.of(requester.getId(), success));
+    private CompletableFuture<Void> sendEmailAsync(final RecruitmentNotificationRequester requester,
+                                                   final EmailContent emailContent) {
+        return CompletableFuture.runAsync(() ->
+                sesEmailSender.sendRawMessageBody(
+                        requester.getEmail(),
+                        emailContent.htmlBody(),
+                        emailContent.subject()
+                ));
     }
 }
