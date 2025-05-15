af2f884fe1f01c2620496762b7e9d75a793a0e51
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-05-16 00:43:50 +0900

    [COT-264] Feature: 메일 전송 API 구현 (#353)
    * build: ses 빌드 파일 작성
    
    * feat: SesConfig 작성
    
    * feat: raw html으로 Ses 메일 전송 로직 구현
    
    * feat: 비동기 메일 전송 로직 구현
    
    * feat: 비동기 시 결과를 결과 반환하게 변경
    
    * feat: 메일 전송 서비스 로직 구현
    
    * refactor: Batch size에 맞게 나눠서 저장하게 수정
    
    * feat: 메일 제목+html바디 return하는 로직 구현
    
    * feat: 모집 알림 메일 html 수정
    
    * refactor: Ses 메일 전송 컨포넌트명 변경
    
    * chore: 메일 전송 주소 QA, Main 다르게 설정할 수 있게 yaml파일 변경
    
    * refactor: 메일 전송 비동기처리 설정 변경
    
    * feat: 모집 알림 메일 홈페이지 링크 색 수정
    
    * feat: 모집 알림 메일 포매팅 수정
    
    * refactor: 메일 전송 링크 환경변수로 주입받게 변경
    
    * refactor: 모집 알람 메일 html 받는 메소드명 변경
    
    * feat: 레파지토리 트랜잭션 어노테이션 추가
    
    * feat: 로그 저장 메소드 트랜잭션 어노테이션 추가
    
    * refactor: 메일 전송시 emailContent를 통으로 보내게 변경
    
    * refactor: 람다식 변수명 풀어서 작성
    
    * refactor: RecruitmentNotificationRequester map 변수명 변경
    
    * refactor: RecruitmentNotificationRequester map 변수명 변경
    
    * refactor: run-async, exceptionally로 메일 전송 로직 수정
    
    * chore: base-url 공통 yml로 이동
    
    * chore: ses email 주소 공통 yml로 이동
    
    * feat: 홈페이지 주소 DI 주입으로 변경
    
    * refactor: AWS 관련 환경변수 Bean으로 등록하게 변경
    
    * refactor: 전송 메일 주소를 받을 때 AwsProperties를 통해 받도록 변경
    
    * refactor: aws region 환경변수명 변경
    
    * chore: SES 이메일 주소 CI/CD 스크립트 설정
    
    * chore: 홈페이지 주소 CI/CD 스크립트 설정
    
    * refactor: Map 변수명 변경
    
    * chore: ses 이메일 주소 CI/CD 스크립트 secret 변수명 변경
    
    * style: AwsProperties 주석 제거
    
    * test: 메일 전송 테스트 코드 작성
    
    (cherry picked from commit 6a594ab8ece3f35d103b53dd55a2eaad9254f062)
    (cherry picked from commit ca20975fc90a05d15909bf18f29506d7be6f978e)

--- src/main/java/org/cotato/csquiz/domain/recruitment/repository/RecruitmentNotificationRequesterRepository.java
+++ src/main/java/org/cotato/csquiz/domain/recruitment/repository/RecruitmentNotificationRequesterRepository.java
@@ -1,10 +1,22 @@
 package org.cotato.csquiz.domain.recruitment.repository;
 
+import java.util.List;
 import org.cotato.csquiz.domain.recruitment.entity.RecruitmentNotificationRequester;
 import org.cotato.csquiz.domain.recruitment.enums.SendStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface RecruitmentNotificationRequesterRepository extends
         JpaRepository<RecruitmentNotificationRequester, Long> {
     boolean existsByEmailAndSendStatus(String recruitEmail, SendStatus sendStatus);
+
+    List<RecruitmentNotificationRequester> findAllBySendStatusIn(List<SendStatus> status);
+
+    @Transactional
+    @Modifying
+    @Query("update RecruitmentNotificationRequester r set r.sendStatus = :status where r.id in :ids")
+    void updateSendStatusByIds(@Param("status") SendStatus sendStatus, @Param("ids") List<Long> ids);
 }
