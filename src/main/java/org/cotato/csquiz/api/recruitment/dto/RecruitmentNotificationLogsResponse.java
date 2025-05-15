27c395207b749aa92e830abd218f968b1e7e8899
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-05-16 01:23:25 +0900

    [COT-265] Feature: 모집 알람 전송 기록 반환 API (#357)
    * feat: 전송 기록 반환 API
    
    * feat: DB에서 모집 기록과 멤버를 join해서 반환하도록 변경
    
    * test: 전송 기록 반환 테스트 코드 작성
    
    * refactor: HashMap이 아닌 Map으로 적용
    
    * refactor: NotificationId로 Grouping하는 로직을 implements 레이어로 이동
    
    * refactor: 로그 최대 갯수 상수화
    
    * refactor: 5개의 모집 알람을 가져올 때 Page를 이용하지 않게 변경
    
    * test: Grouping 변경에 따른 테스트 코드 수정
    
    * refactor: 한 로그의 response 클래스명 변경
    
    * refactor: response 전송자 변수명 변경
    
    sender -> senderName
    
    * test: 테스트 코드 변경
    
    * refactor: groupBy 파라미터 간소화
    
    * refactor: 로그응답 리스트 변수명 변경
    
    dto -> responses
    
    (cherry picked from commit d65efd335763aed0fc1d0566843712cb8dbf2b7a)

---
+++ src/main/java/org/cotato/csquiz/api/recruitment/dto/RecruitmentNotificationLogsResponse.java
@@ -0,0 +1,11 @@
+package org.cotato.csquiz.api.recruitment.dto;
+
+import java.util.List;
+
+public record RecruitmentNotificationLogsResponse(
+        List<RecruitmentNotificationLogResponse> notificationLogs
+) {
+    public static RecruitmentNotificationLogsResponse of(List<RecruitmentNotificationLogResponse> dtos) {
+        return new RecruitmentNotificationLogsResponse(dtos);
+    }
+}
