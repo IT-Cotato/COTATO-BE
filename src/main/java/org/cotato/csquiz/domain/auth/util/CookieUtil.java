185c17e5fb9d0fe0c059472199f42b16b57d96b8
youth <boysoeng@naver.com>
2025-06-07 18:01:58 +0900

    feat: 쿠키 생성 유틸 클래스 구현

---
+++ src/main/java/org/cotato/csquiz/domain/auth/util/CookieUtil.java
@@ -0,0 +1,25 @@
+package org.cotato.csquiz.domain.auth.util;
+
+import static org.cotato.csquiz.domain.auth.constant.TokenConstants.REFRESH_TOKEN;
+
+import jakarta.servlet.http.Cookie;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public final class CookieUtil {
+
+    private static final int COOKIE_MAX_AGE = 60 * 60 * 24 * 3;
+
+    public static Cookie createRefreshCookie(final String refreshToken) {
+        Cookie refreshCookie = new Cookie(REFRESH_TOKEN, refreshToken);
+        refreshCookie.setMaxAge(COOKIE_MAX_AGE);
+        refreshCookie.setPath("/");
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setSecure(true);
+
+        log.info("[리프레시 쿠키 발급, 발급시간 : {}]", LocalDateTime.now());
+
+        return refreshCookie;
+    }
+}
