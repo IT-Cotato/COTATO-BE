f896b3a12a8c0d8eafab744acbd2a6d332503aab
Youth <109585620+Youthhing@users.noreply.github.com>
2025-06-12 13:27:59 +0900

    [COT-285] Feature: 세션 이미지 생성 이벤트 처리 (#362)
    * refactor: 세션 생성 API 파라미터 정리
    
    * feat: add not null time validation
    
    * feat: session 생성 시 세션 이미지 업데이트 이벤트 발행
    
    * test: 세션 이미지 수정 이벤트 발행 테스트
    
    * test: 세션 생성 테스트
    
    * chore: remove method signature
    
    * feat: remove constraint
    
    * feat: attendance create event
    
    * feat: use attendance create event
    
    * test: fix test error
    
    * chore: remove unused import line
    
    * chore: check null with equals
    
    ---------
    
    Co-authored-by: gikhoon <gikhoon@gmail.com>
    (cherry picked from commit 14b1e81dab21ae8f624e90f674e7089039747ca4)

--- src/main/java/org/cotato/csquiz/common/event/EventType.java
+++ src/main/java/org/cotato/csquiz/common/event/EventType.java
@@ -8,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 public enum EventType {
 
     APPROVE_MEMBER("부원 가입 승인"),
-    REJECT_MEMBER("부원 가입 거절");
+    REJECT_MEMBER("부원 가입 거절"),
+    SESSION_IMAGE_UPDATE("세션 이미지 업데이트"),
+    ATTENDANCE_CREATE("출석 생성"),
+    ;
 
     private final String description;
 }
