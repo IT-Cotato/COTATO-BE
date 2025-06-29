2ade46248bc332277eecaf8c7b7bb5406ea4a38d
Youth <109585620+Youthhing@users.noreply.github.com>
2025-06-30 00:02:56 +0900

    [COT-293] Fix: 세션 추가 실패 및 예기치 못한 예외 처리 (#367)
    * feat: add session date time
    
    * chore: 예기치 못한 에러 발생 시 처리
    
    (cherry picked from commit f2ace32bd46081d767b68d43e1028dd98b580548)

---
+++ src/test/java/org/cotato/csquiz/common/error/handler/GlobalExceptionHandlerTest.java
@@ -0,0 +1,37 @@
+package org.cotato.csquiz.common.error.handler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Objects;
+import org.cotato.csquiz.common.error.ErrorCode;
+import org.cotato.csquiz.common.error.response.ErrorResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+@ExtendWith(MockitoExtension.class)
+class GlobalExceptionHandlerTest {
+
+    @InjectMocks
+    private GlobalExceptionHandler globalExceptionHandler;
+
+    @Test
+    @DisplayName("예상치 못한 에러 발생 시 예외 처리")
+    void handleUnexpectedException() {
+        // Given
+        Exception exception = new Exception("Unexpected error");
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        // When
+        ResponseEntity<ErrorResponse> response = globalExceptionHandler.handleException(exception,
+                request);
+
+        // Then
+        assertEquals(500, response.getStatusCode().value());
+        assertEquals(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), Objects.requireNonNull(response.getBody()).message());
+    }
+}
