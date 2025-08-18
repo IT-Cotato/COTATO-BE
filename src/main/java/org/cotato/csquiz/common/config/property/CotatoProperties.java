67c7faba39850217bf03755f7e5c681db1c2e14a
youth <boysoeng@naver.com>
2025-08-19 00:39:26 +0900

    feat: 코테이토 도메인 환경 변수 가져오기

--- src/main/java/org/cotato/csquiz/common/config/property/CotatoProperties.java
+++ src/main/java/org/cotato/csquiz/common/config/property/CotatoProperties.java
@@ -1,18 +1,22 @@
 package org.cotato.csquiz.common.config.property;
 
+import java.util.List;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Setter
 @Component
 @ConfigurationProperties(prefix = "cotato")
 public class CotatoProperties {
-    private String baseUrl;
 
-    public String getBaseUrl() {
+    private List<String> baseUrl;
+
+    public List<String> getBaseUrls() {
         return baseUrl;
     }
 
-    public void setBaseUrl(String baseUrl) {
-        this.baseUrl = baseUrl;
+    public String getBaseUrl() {
+        return baseUrl.get(0);
     }
 }
